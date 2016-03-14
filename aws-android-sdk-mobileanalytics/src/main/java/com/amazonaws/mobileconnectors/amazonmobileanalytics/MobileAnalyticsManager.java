0becd5ffef59197bc1f21795947ef7743517034d
Tal Shargal <tal@Tals-MacBook-Air-2.local>
2016-03-14 14:50:13 +0200

    getOrCreateInstance method added with AnalyticsConfig as a paremeter

--- aws-android-sdk-mobileanalytics/src/main/java/com/amazonaws/mobileconnectors/amazonmobileanalytics/MobileAnalyticsManager.java
+++ aws-android-sdk-mobileanalytics/src/main/java/com/amazonaws/mobileconnectors/amazonmobileanalytics/MobileAnalyticsManager.java
@@ -247,6 +247,24 @@ public class MobileAnalyticsManager {
     }
 
     /*
+     * Same as getOrCreateInstance(Context, String, Regions, AWSCredentialsProvider, AnalyticsConfig)
+     * without the Regions and AWSCredentialsProvider parameters. This method defaults
+     * to use Regions.US_EAST_1 and create a CognitoCachingCredentialsProvider with the
+     * provided Cognito identity pool Id.
+     */
+    public static MobileAnalyticsManager getOrCreateInstance(Context context, String appId,
+            String cognitoId, AnalyticsConfig options
+            ) throws InitializationException {
+
+        CognitoCachingCredentialsProvider cognitoProvider = new CognitoCachingCredentialsProvider(
+                context,
+                cognitoId,
+                Regions.US_EAST_1
+        );
+        return getOrCreateInstance(context, appId, Regions.US_EAST_1, cognitoProvider, options, null);
+    }
+
+    /*
      * Same as getOrCreateInstance(Context, String, Regions, AWSCredentialsProvider,
      * AnalyticsConfig) without the AnalyticsConfig options parameter.
      */
