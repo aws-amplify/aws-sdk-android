e5f07517eae290c3cc61721714c072f12c60278d
AWS <aws-mobile-sdk@amazon.com>
2017-12-21 21:46:09 +0000

    AWS SDK for Android 2.6.12

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.6.11", VersionInfoUtils.getVersion());
+        assertEquals("2.6.12", VersionInfoUtils.getVersion());
     }
 
     @Test
