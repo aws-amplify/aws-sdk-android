766b13dd892a7da24c41a1c525da7f65da2afeb7
AWS <aws-mobile-sdk@amazon.com>
2018-10-11 17:28:12 +0000

    AWS SDK for Android 2.7.5

--- aws-android-sdk-auth-ui/src/main/java/com/amazonaws/mobile/auth/ui/SignInView.java
+++ aws-android-sdk-auth-ui/src/main/java/com/amazonaws/mobile/auth/ui/SignInView.java
@@ -58,7 +58,7 @@ public class SignInView extends LinearLayout {
     private static final int IMAGE_LAYOUT_MARGINS = dp(10);
 
     /** String that represents the SDK Version. */
-    private static final String SDK_VERSION = "2.7.4";
+    private static final String SDK_VERSION = "2.7.5";
 
     /** Common Prefix of the namespaces of different SignIn providers. */
     private static final String NAMESPACE_COMMON_PREFIX = "com.amazonaws.mobile.auth";
