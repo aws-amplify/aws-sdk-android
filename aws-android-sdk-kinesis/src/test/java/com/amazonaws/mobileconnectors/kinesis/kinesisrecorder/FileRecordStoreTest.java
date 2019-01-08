d118b1b30f6985df73b1be04c34d590f84719f0c
sunchunqiang <sdechunq@amazon.com>
2019-01-08 09:26:31 -0800

    only first 3

--- aws-android-sdk-kinesis/src/test/java/com/amazonaws/mobileconnectors/kinesis/kinesisrecorder/FileRecordStoreTest.java
+++ aws-android-sdk-kinesis/src/test/java/com/amazonaws/mobileconnectors/kinesis/kinesisrecorder/FileRecordStoreTest.java
@@ -154,125 +154,125 @@ public class FileRecordStoreTest {
 
     }
 
-    // @Test
-    // public void
-    //         testWhenRecordssFileIsMissingAndRecordssDirectoryIsReadOnly_error() throws IOException {
-    //     File kinesisDirectory = null;
-    //     try {
-    //         File eventDirectory = null;
+    @Test
+    public void
+            testWhenRecordssFileIsMissingAndRecordssDirectoryIsReadOnly_error() throws IOException {
+        File kinesisDirectory = null;
+        try {
+            File eventDirectory = null;
 
-    //         FileManager fileManager = new FileManager(TEST_DIRECTORY);
-    //         FileRecordStore recordStore = new FileRecordStore(TEST_DIRECTORY,
-    //                 RECORDER_FILE_NAME, MAX_STORAGE_SIZE);
+            FileManager fileManager = new FileManager(TEST_DIRECTORY);
+            FileRecordStore recordStore = new FileRecordStore(TEST_DIRECTORY,
+                    RECORDER_FILE_NAME, MAX_STORAGE_SIZE);
 
-    //         kinesisDirectory =
-    //                 fileManager.createDirectory(Constants.RECORDS_DIRECTORY);
-    //         File recordsFile = new File(eventDirectory, Constants.RECORDS_FILE_NAME);
+            kinesisDirectory =
+                    fileManager.createDirectory(Constants.RECORDS_DIRECTORY);
+            File recordsFile = new File(eventDirectory, Constants.RECORDS_FILE_NAME);
 
-    //         fileManager.deleteFile(recordsFile);
-    //         kinesisDirectory.setReadOnly();
-    //         recordStore.put("2");
-    //     } finally {
-    //         if (kinesisDirectory != null && kinesisDirectory.exists()) {
+            fileManager.deleteFile(recordsFile);
+            kinesisDirectory.setReadOnly();
+            recordStore.put("2");
+        } finally {
+            if (kinesisDirectory != null && kinesisDirectory.exists()) {
 
-    //             kinesisDirectory.setWritable(true);
-    //             kinesisDirectory.delete();
-    //         }
-    //     }
+                kinesisDirectory.setWritable(true);
+                kinesisDirectory.delete();
+            }
+        }
 
-    // }
+    }
 
-    // @Test
-    // public void testWhenWritingExceedsMaxStorageSize_noMoreRecordsWritten() throws IOException {
-    //     File recordsDirectory = null;
-    //     File recordsFile = null;
+    @Test
+    public void testWhenWritingExceedsMaxStorageSize_noMoreRecordsWritten() throws IOException {
+        File recordsDirectory = null;
+        File recordsFile = null;
 
-    //     FileManager fileManager = new FileManager(TEST_DIRECTORY);
-    //     FileRecordStore recordStore = new FileRecordStore(TEST_DIRECTORY,
-    //             RECORDER_FILE_NAME, 100L);
+        FileManager fileManager = new FileManager(TEST_DIRECTORY);
+        FileRecordStore recordStore = new FileRecordStore(TEST_DIRECTORY,
+                RECORDER_FILE_NAME, 100L);
 
-    //     recordsDirectory = fileManager.getDirectory(Constants.RECORDS_DIRECTORY);
-    //     recordsFile = new File(recordsDirectory, Constants.RECORDS_FILE_NAME);
+        recordsDirectory = fileManager.getDirectory(Constants.RECORDS_DIRECTORY);
+        recordsFile = new File(recordsDirectory, Constants.RECORDS_FILE_NAME);
 
-    //     for (int i = 0; i < 10; i++) {
-    //         recordStore.put("ten bytes");
-    //     }
-    //     assertSame(recordsFile.length(), 100L);
+        for (int i = 0; i < 10; i++) {
+            recordStore.put("ten bytes");
+        }
+        assertSame(recordsFile.length(), 100L);
 
-    //     recordStore.put("0123456789");
-    //     assertSame(recordsFile.length(), 100L);
+        recordStore.put("0123456789");
+        assertSame(recordsFile.length(), 100L);
 
-    // }
+    }
 
-    // @Test
-    // public void testWhenWritingTooManyConcurrentRecords() throws
-    //         InterruptedException, IOException {
-    //     File recordsDirectory = null;
-    //     File recordsFile = null;
-    //     FileManager fileManager = new FileManager(TEST_DIRECTORY);
-    //     final FileRecordStore recordStore = new FileRecordStore(TEST_DIRECTORY,
-    //             RECORDER_FILE_NAME, MAX_STORAGE_SIZE);
+    @Test
+    public void testWhenWritingTooManyConcurrentRecords() throws
+            InterruptedException, IOException {
+        File recordsDirectory = null;
+        File recordsFile = null;
+        FileManager fileManager = new FileManager(TEST_DIRECTORY);
+        final FileRecordStore recordStore = new FileRecordStore(TEST_DIRECTORY,
+                RECORDER_FILE_NAME, MAX_STORAGE_SIZE);
 
-    //     recordsDirectory = fileManager.getDirectory(Constants.RECORDS_DIRECTORY);
-    //     recordsFile = new File(recordsDirectory, Constants.RECORDS_FILE_NAME);
+        recordsDirectory = fileManager.getDirectory(Constants.RECORDS_DIRECTORY);
+        recordsFile = new File(recordsDirectory, Constants.RECORDS_FILE_NAME);
 
-    //     SecureRandom random = new SecureRandom();
+        SecureRandom random = new SecureRandom();
 
-    //     // first fill the disk
-    //     String tempRecordStr = "";
-    //     for (int i = 0; i < 10000; i++) {
-    //         tempRecordStr = tempRecordStr + new BigInteger(130, random).toString(32);
-    //     }
+        // first fill the disk
+        String tempRecordStr = "";
+        for (int i = 0; i < 10000; i++) {
+            tempRecordStr = tempRecordStr + new BigInteger(130, random).toString(32);
+        }
 
-    //     final String recordStr = tempRecordStr;
+        final String recordStr = tempRecordStr;
 
-    //     for (int i = 0; i < 30; i++) {
-    //         recordStore.put(recordStr);
-    //     }
+        for (int i = 0; i < 30; i++) {
+            recordStore.put(recordStr);
+        }
 
-    //     long initialSize = recordsFile.length();
-    //     assertTrue(recordsFile.length() <= MAX_STORAGE_SIZE);
+        long initialSize = recordsFile.length();
+        assertTrue(recordsFile.length() <= MAX_STORAGE_SIZE);
 
-    //     final CountDownLatch latch = new CountDownLatch(1);
-    //     ExecutorService threadPool = Executors.newFixedThreadPool(1);
-    //     threadPool.execute(new Runnable() {
-    //         @Override
-    //         public void run() {
-    //             try {
-    //                 for (int i = 0; i < 100; i++) {
-    //                     RecordIterator itr = recordStore.iterator();
-    //                     if (itr.hasNext()) {
-    //                         String next = itr.next();
-    //                         assertEquals(next.length(), recordStr.length());
-    //                         itr.removeReadRecords();
-    //                     }
-    //                     Thread.sleep(1);
-    //                 }
-    //             } catch (InterruptedException e) {
-    //                 e.printStackTrace();
-    //             } catch (IOException e) {
-    //                 throw new RuntimeException(e);
-    //             } finally {
-    //                 latch.countDown();
-    //             }
-    //         }
-    //     });
+        final CountDownLatch latch = new CountDownLatch(1);
+        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 100; i++) {
+                        RecordIterator itr = recordStore.iterator();
+                        if (itr.hasNext()) {
+                            String next = itr.next();
+                            assertEquals(next.length(), recordStr.length());
+                            itr.removeReadRecords();
+                        }
+                        Thread.sleep(1);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
 
-    //     for (int i = 0; i < 10000; i++) {
-    //         recordStore.put(recordStr);
-    //         assertTrue(recordsFile.length() <= initialSize);
-    //         recordStore.put(recordStr);
-    //         assertTrue(recordsFile.length() <= initialSize);
-    //         recordStore.put(recordStr);
-    //         assertTrue(recordsFile.length() <= initialSize);
-    //         Thread.sleep(1);
-    //     }
+        for (int i = 0; i < 10000; i++) {
+            recordStore.put(recordStr);
+            assertTrue(recordsFile.length() <= initialSize);
+            recordStore.put(recordStr);
+            assertTrue(recordsFile.length() <= initialSize);
+            recordStore.put(recordStr);
+            assertTrue(recordsFile.length() <= initialSize);
+            Thread.sleep(1);
+        }
 
-    //     latch.await();
-    //     assertEquals(recordsFile.length(), initialSize);
-    //     assertTrue(recordsFile.length() < MAX_STORAGE_SIZE);
+        latch.await();
+        assertEquals(recordsFile.length(), initialSize);
+        assertTrue(recordsFile.length() < MAX_STORAGE_SIZE);
 
-    // }
+    }
 
     // @Test
     // public void
