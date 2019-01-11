c9e088b6d7e08b6d9c010bb3a16fe2d7b4ba84f8
Chunqiang SUN <sdechunq@amazon.com>
2019-01-11 14:45:14 -0800

    Configure CircleCI  (#642)

---
+++ CircleciScripts/run_unittest.py
@@ -0,0 +1,20 @@
+from functions import runcommand
+from functions import runtest
+from functions import TestTypes
+from functions import getmodules
+import sys
+import os
+
+test_results = sys.argv[1]
+root = sys.argv[2]
+print(root)
+testmodules =  getmodules(root)
+
+
+runcommand("rm -rf {0}".format(test_results))
+runcommand("mkdir {0}".format(test_results))
+for module in testmodules:
+    testfolder = os.path.join(root, module, "src/test")
+    if (os.path.isdir(testfolder)):
+        if runtest(module, TestTypes.UnitTest, test_results) != 0 :
+            exit(1)
