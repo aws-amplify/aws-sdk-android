7a2955cd387dfdcd292f7f5eede4b6b32c8a2f84
AWS <aws-mobile-sdk@amazon.com>
2019-01-11 23:23:32 +0000

    AWS SDK for Android 2.10.1

--- CircleciScripts/check_testresult.sh
+++
@@ -1,7 +0,0 @@
-
-target="$1"
-
-if find $target -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
-    echo "$target is not empty. There are test failures"
-    exit 1
-fi
--- aws-android-sdk-pinpoint/src/main/java/com/amazonaws/mobileconnectors/pinpoint/internal/event/EventRecorder.java
+++ aws-android-sdk-pinpoint/src/main/java/com/amazonaws/mobileconnectors/pinpoint/internal/event/EventRecorder.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2016-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -580,10 +580,10 @@ public class EventRecorder {
         event.withAppPackageName(appDetails.packageName())
                 .withAppTitle(appDetails.getAppTitle())
                 .withAppVersionCode(appDetails.versionCode())
+                .withAttributes(internalEvent.getAllAttributes())
                 .withClientSdkVersion(internalEvent.getSdkVersion())
                 .withEventType(internalEvent.getEventType())
                 .withMetrics(internalEvent.getAllMetrics())
-                .withAttributes(internalEvent.getAllAttributes())
                 .withSdkName(internalEvent.getSdkName())
                 .withSession(session)
                 .withTimestamp(DateUtils.formatISO8601Date(new Date(internalEvent.getEventTimestamp())));
