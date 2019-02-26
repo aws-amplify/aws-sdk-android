55622838b0ef98f67efd9ff25626b4e55d407c83
Roshan Kumar <roskumr@amazon.com>
2019-02-26 15:43:59 -0800

    Integration test for aws-android-sdk-apigateway-core module (#728)
    * Integration tests for aws-android-sdk-apigateway module
    
    * Update run_integrationtest.py
    
    Add API Gateway to the list

---
+++ aws-android-sdk-apigateway-test/src/androidTest/java/com/amazonaws/mobileconnectors/apigateway/ApiGatewayIntegrationTestBase.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazonaws.mobileconnectors.apigateway;
+
+import android.support.test.InstrumentationRegistry;
+
+import com.amazonaws.apigatewaytest.R;
+import com.amazonaws.testutils.AWSTestBase;
+
+import org.json.JSONObject;
+
+public abstract class ApiGatewayIntegrationTestBase extends AWSTestBase {
+
+    /** Package name in testconfiguration.json */
+    protected static final String PACKAGE_NAME = InstrumentationRegistry.getTargetContext().
+                                                    getResources().getString(R.string.package_name);
+
+    public static JSONObject getPackageConfigure()  {
+        return getPackageConfigure(PACKAGE_NAME);
+    }
+}
