e17e1c009853731cfa8a3375eca6224a728b1f6f
Roshan Kumar <roskumr@amazon.com>
2019-03-01 10:36:35 -0800

    Bump the patch version

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.12.1", VersionInfoUtils.getVersion());
+        assertEquals("2.12.2", VersionInfoUtils.getVersion());
     }
 
     @Test
