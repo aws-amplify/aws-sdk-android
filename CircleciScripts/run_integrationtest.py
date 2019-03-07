ab1575cedd1b1a2833d9044ceba187994173c40f
Karthikeyan Vasuki Balasubramaniam <karthba@amazon.com>
2019-03-06 23:18:01 -0800

    Enable core, cognitoidentityprovider and cognitoauth integration tests on CircleCI

--- CircleciScripts/run_integrationtest.py
+++ CircleciScripts/run_integrationtest.py
@@ -9,7 +9,7 @@ credentials = sys.argv[3]
 testmodules =  ["aws-android-sdk-apigateway-test",
                 "aws-android-sdk-autoscaling-test",
                 "aws-android-sdk-cognitoidentityprovider-test",
-                "aws-android-sdk-cognitoauth-test",
+                "aws-android-sdk-cognitoauth",
                 "aws-android-sdk-core-test",
                 "aws-android-sdk-cloudwatch-test",
                 "aws-android-sdk-elb-test",
