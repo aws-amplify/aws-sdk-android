e8c2d7ec3ba982bce2522e688a08b15faffff6e2
Karthikeyan Vasuki Balasubramaniam <karthba@amazon.com>
2019-03-07 12:48:04 -0800

    [2.12.3] Bump the patch version of 2.12.z

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.12.2", VersionInfoUtils.getVersion());
+        assertEquals("2.12.3", VersionInfoUtils.getVersion());
     }
 
     @Test
