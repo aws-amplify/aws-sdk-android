625b1f5a818cde47d1c74f5be66eef7799206f50
Min Bi <bimin@amazon.com>
2019-04-01 15:29:13 +0000

    [aws-android-sdk-cognitoidentityprovider] Update models to latest

---
+++ aws-android-sdk-cognitoidentityprovider/src/main/java/com/amazonaws/services/cognitoidentityprovider/model/ListTagsForResourceRequest.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazonaws.services.cognitoidentityprovider.model;
+
+import java.io.Serializable;
+
+import com.amazonaws.AmazonWebServiceRequest;
+
+/**
+ * <p>
+ * Lists the tags that are assigned to an Amazon Cognito user pool.
+ * </p>
+ * <p>
+ * A tag is a label that you can apply to user pools to categorize and manage
+ * them in different ways, such as by purpose, owner, environment, or other
+ * criteria.
+ * </p>
+ * <p>
+ * You can use this action up to 10 times per second, per account.
+ * </p>
+ */
+public class ListTagsForResourceRequest extends AmazonWebServiceRequest implements Serializable {
+    /**
+     * <p>
+     * The Amazon Resource Name (ARN) of the user pool that the tags are
+     * assigned to.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>20 - 2048<br/>
+     * <b>Pattern:
+     * </b>arn:[\w+=/,.@-]+:[\w+=/,.@-]+:([\w+=/,.@-]*)?:[0-9]+:[\w+=
+     * /,.@-]+(:[\w+=/,.@-]+)?(:[\w+=/,.@-]+)?<br/>
+     */
+    private String resourceArn;
+
+    /**
+     * <p>
+     * The Amazon Resource Name (ARN) of the user pool that the tags are
+     * assigned to.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>20 - 2048<br/>
+     * <b>Pattern:
+     * </b>arn:[\w+=/,.@-]+:[\w+=/,.@-]+:([\w+=/,.@-]*)?:[0-9]+:[\w+=
+     * /,.@-]+(:[\w+=/,.@-]+)?(:[\w+=/,.@-]+)?<br/>
+     *
+     * @return <p>
+     *         The Amazon Resource Name (ARN) of the user pool that the tags are
+     *         assigned to.
+     *         </p>
+     */
+    public String getResourceArn() {
+        return resourceArn;
+    }
+
+    /**
+     * <p>
+     * The Amazon Resource Name (ARN) of the user pool that the tags are
+     * assigned to.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>20 - 2048<br/>
+     * <b>Pattern:
+     * </b>arn:[\w+=/,.@-]+:[\w+=/,.@-]+:([\w+=/,.@-]*)?:[0-9]+:[\w+=
+     * /,.@-]+(:[\w+=/,.@-]+)?(:[\w+=/,.@-]+)?<br/>
+     *
+     * @param resourceArn <p>
+     *            The Amazon Resource Name (ARN) of the user pool that the tags
+     *            are assigned to.
+     *            </p>
+     */
+    public void setResourceArn(String resourceArn) {
+        this.resourceArn = resourceArn;
+    }
+
+    /**
+     * <p>
+     * The Amazon Resource Name (ARN) of the user pool that the tags are
+     * assigned to.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>20 - 2048<br/>
+     * <b>Pattern:
+     * </b>arn:[\w+=/,.@-]+:[\w+=/,.@-]+:([\w+=/,.@-]*)?:[0-9]+:[\w+=
+     * /,.@-]+(:[\w+=/,.@-]+)?(:[\w+=/,.@-]+)?<br/>
+     *
+     * @param resourceArn <p>
+     *            The Amazon Resource Name (ARN) of the user pool that the tags
+     *            are assigned to.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     */
+    public ListTagsForResourceRequest withResourceArn(String resourceArn) {
+        this.resourceArn = resourceArn;
+        return this;
+    }
+
+    /**
+     * Returns a string representation of this object; useful for testing and
+     * debugging.
+     *
+     * @return A string representation of this object.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        if (getResourceArn() != null)
+            sb.append("ResourceArn: " + getResourceArn());
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hashCode = 1;
+
+        hashCode = prime * hashCode
+                + ((getResourceArn() == null) ? 0 : getResourceArn().hashCode());
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+
+        if (obj instanceof ListTagsForResourceRequest == false)
+            return false;
+        ListTagsForResourceRequest other = (ListTagsForResourceRequest) obj;
+
+        if (other.getResourceArn() == null ^ this.getResourceArn() == null)
+            return false;
+        if (other.getResourceArn() != null
+                && other.getResourceArn().equals(this.getResourceArn()) == false)
+            return false;
+        return true;
+    }
+}
