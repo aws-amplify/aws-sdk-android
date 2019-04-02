b6a273dc2b548e017cad22f8c7450e72099d0223
Roshan Kumar <roskumr@amazon.com>
2019-04-02 15:29:08 -0700

    Revert "Add TLS ALPN support for AWS IoT MQTT connections (#779)"
    This reverts commit 3526a58b6a54da5c8a4635845cd1c7bb6c927b39.

---
+++ aws-android-sdk-iot/src/test/java/com/amazonaws/mobileconnectors/iot/IntegrationTestBase.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *    http://aws.amazon.com/apache2.0
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amazonaws.mobileconnectors.iot;
+
+public class IntegrationTestBase {
+
+    // testing credentials constants
+    protected static final String IDENTITY_POOL_ID = "redacted-iot-identity-pool";
+}
