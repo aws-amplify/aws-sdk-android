7556fbdbac0e908927782995a21435b139e17572
Roshan Kumar <roskumr@amazon.com>
2019-04-02 14:12:01 -0700

    Update AWSIotMqttManager to respect the SDK's global time offset (#853)
    * Update AWSIotMqttManager to respect the SDK's global time offset
    
    * Chagelog update

--- aws-android-sdk-iot/src/main/java/com/amazonaws/mobileconnectors/iot/AWSIotMqttManager.java
+++ aws-android-sdk-iot/src/main/java/com/amazonaws/mobileconnectors/iot/AWSIotMqttManager.java
@@ -21,6 +21,7 @@ import android.os.HandlerThread;
 import android.os.Looper;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.SDKGlobalConfiguration;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.util.StringUtils;
@@ -795,7 +796,7 @@ public class AWSIotMqttManager {
                 try {
                     final String mqttWebSocketURL = signer.getSignedUrl(endpointWithHttpPort, 
                         clientCredentialsProvider.getCredentials(),
-                        System.currentTimeMillis());
+                        System.currentTimeMillis() - SDKGlobalConfiguration.getGlobalTimeOffset() * MILLIS_IN_ONE_SECOND);
 
                     final MqttConnectOptions options = new MqttConnectOptions();
 
