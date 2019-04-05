3e5f59e1532d2ca50a321de0b1a911b42f0011d2
awsmobilesdk <46607340+awsmobilesdk@users.noreply.github.com>
2019-04-05 12:07:34 -0700

    [skip test][bump version 2.13.1] (#877)

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.13.0", VersionInfoUtils.getVersion());
+        assertEquals("2.13.1", VersionInfoUtils.getVersion());
     }
 
     @Test
