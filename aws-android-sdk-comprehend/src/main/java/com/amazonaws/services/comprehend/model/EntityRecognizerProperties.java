caeb616d2ff5e4d193b47d2b196f0aaa3714f9b1
Min Bi <bimin@amazon.com>
2019-05-19 13:02:44 +0000

    [aws-android-sdk-comprehend] Update models to latest

--- aws-android-sdk-comprehend/src/main/java/com/amazonaws/services/comprehend/model/EntityRecognizerProperties.java
+++ aws-android-sdk-comprehend/src/main/java/com/amazonaws/services/comprehend/model/EntityRecognizerProperties.java
@@ -147,6 +147,17 @@ public class EntityRecognizerProperties implements Serializable {
 
     /**
      * <p>
+     * Configuration parameters for a private Virtual Private Cloud (VPC)
+     * containing the resources you are using for your custom entity recognizer.
+     * For more information, see <a href=
+     * "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     * >Amazon VPC</a>.
+     * </p>
+     */
+    private VpcConfig vpcConfig;
+
+    /**
+     * <p>
      * The Amazon Resource Name (ARN) that identifies the entity recognizer.
      * </p>
      * <p>
@@ -956,6 +967,75 @@ public class EntityRecognizerProperties implements Serializable {
     }
 
     /**
+     * <p>
+     * Configuration parameters for a private Virtual Private Cloud (VPC)
+     * containing the resources you are using for your custom entity recognizer.
+     * For more information, see <a href=
+     * "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     * >Amazon VPC</a>.
+     * </p>
+     *
+     * @return <p>
+     *         Configuration parameters for a private Virtual Private Cloud
+     *         (VPC) containing the resources you are using for your custom
+     *         entity recognizer. For more information, see <a href=
+     *         "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     *         >Amazon VPC</a>.
+     *         </p>
+     */
+    public VpcConfig getVpcConfig() {
+        return vpcConfig;
+    }
+
+    /**
+     * <p>
+     * Configuration parameters for a private Virtual Private Cloud (VPC)
+     * containing the resources you are using for your custom entity recognizer.
+     * For more information, see <a href=
+     * "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     * >Amazon VPC</a>.
+     * </p>
+     *
+     * @param vpcConfig <p>
+     *            Configuration parameters for a private Virtual Private Cloud
+     *            (VPC) containing the resources you are using for your custom
+     *            entity recognizer. For more information, see <a href=
+     *            "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     *            >Amazon VPC</a>.
+     *            </p>
+     */
+    public void setVpcConfig(VpcConfig vpcConfig) {
+        this.vpcConfig = vpcConfig;
+    }
+
+    /**
+     * <p>
+     * Configuration parameters for a private Virtual Private Cloud (VPC)
+     * containing the resources you are using for your custom entity recognizer.
+     * For more information, see <a href=
+     * "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     * >Amazon VPC</a>.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     *
+     * @param vpcConfig <p>
+     *            Configuration parameters for a private Virtual Private Cloud
+     *            (VPC) containing the resources you are using for your custom
+     *            entity recognizer. For more information, see <a href=
+     *            "https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html"
+     *            >Amazon VPC</a>.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     */
+    public EntityRecognizerProperties withVpcConfig(VpcConfig vpcConfig) {
+        this.vpcConfig = vpcConfig;
+        return this;
+    }
+
+    /**
      * Returns a string representation of this object; useful for testing and
      * debugging.
      *
@@ -989,7 +1069,9 @@ public class EntityRecognizerProperties implements Serializable {
         if (getDataAccessRoleArn() != null)
             sb.append("DataAccessRoleArn: " + getDataAccessRoleArn() + ",");
         if (getVolumeKmsKeyId() != null)
-            sb.append("VolumeKmsKeyId: " + getVolumeKmsKeyId());
+            sb.append("VolumeKmsKeyId: " + getVolumeKmsKeyId() + ",");
+        if (getVpcConfig() != null)
+            sb.append("VpcConfig: " + getVpcConfig());
         sb.append("}");
         return sb.toString();
     }
@@ -1019,6 +1101,7 @@ public class EntityRecognizerProperties implements Serializable {
                 + ((getDataAccessRoleArn() == null) ? 0 : getDataAccessRoleArn().hashCode());
         hashCode = prime * hashCode
                 + ((getVolumeKmsKeyId() == null) ? 0 : getVolumeKmsKeyId().hashCode());
+        hashCode = prime * hashCode + ((getVpcConfig() == null) ? 0 : getVpcConfig().hashCode());
         return hashCode;
     }
 
@@ -1089,6 +1172,11 @@ public class EntityRecognizerProperties implements Serializable {
             return false;
         if (other.getVolumeKmsKeyId() != null
                 && other.getVolumeKmsKeyId().equals(this.getVolumeKmsKeyId()) == false)
+            return false;
+        if (other.getVpcConfig() == null ^ this.getVpcConfig() == null)
+            return false;
+        if (other.getVpcConfig() != null
+                && other.getVpcConfig().equals(this.getVpcConfig()) == false)
             return false;
         return true;
     }
