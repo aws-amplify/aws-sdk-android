67218ba774e6d20a2540b8d5f077367083dbd331
Jithin Roy <royji@amazon.com>
2019-06-21 14:27:48 -0700

    Adding SageMakerRuntime API
        Adding model and service classes to make Sagemaker runtime API
        calls.
        Currently Sagemaker as InvokeEndpoint API.

---
+++ aws-android-sdk-sagemaker-runtime/src/main/java/com/amazonaws/services/sagemakerrts/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes modeling the various types represented by AmazonSageMakerRuntime
+ */
+
+package com.amazonaws.services.sagemakerrts.model;
+
---
+++ aws-android-sdk-sagemaker-runtime/src/main/java/com/amazonaws/services/sagemakerrts/model/transform/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Marhsalling for the various types represented byAmazonSageMakerRuntime
+ */
+
+package com.amazonaws.services.sagemakerrts.model.transform;
+
