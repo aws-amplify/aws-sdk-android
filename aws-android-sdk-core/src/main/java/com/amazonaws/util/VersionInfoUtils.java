79b7c09aa9683c452135f85d7e15392e73f1c2d5
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2019-07-05 11:04:48 -0700

    [bump version 2.13.7] (#1064)

--- aws-android-sdk-core/src/main/java/com/amazonaws/util/VersionInfoUtils.java
+++ aws-android-sdk-core/src/main/java/com/amazonaws/util/VersionInfoUtils.java
@@ -26,7 +26,7 @@ public class VersionInfoUtils {
     private static final int DEFAULT_STRING_LENGTH = 128;
 
     /** SDK version info */
-    private static volatile String version = "2.13.6";
+    private static volatile String version = "2.13.7";
                                                                 // changed build
                                                                 // logic
 
