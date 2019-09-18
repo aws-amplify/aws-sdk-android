988762d33f51cf23fc7ac1dad518fc2a27cd5747
awsmobilesdk <circleci@ea908549885d>
2019-09-18 15:10:11 +0000

    [bump version 2.16.0]

--- aws-android-sdk-core/src/main/java/com/amazonaws/util/VersionInfoUtils.java
+++ aws-android-sdk-core/src/main/java/com/amazonaws/util/VersionInfoUtils.java
@@ -26,7 +26,7 @@ public class VersionInfoUtils {
     private static final int DEFAULT_STRING_LENGTH = 128;
 
     /** SDK version info */
-    private static volatile String version = "2.15.2";
+    private static volatile String version = "2.16.0";
                                                                 // changed build
                                                                 // logic
 
