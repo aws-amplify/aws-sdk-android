318f759b54b916e9f78a86c7ae3375482066ecc2
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2019-11-05 13:19:23 -0800

    [bump version 2.16.3] (#1295)

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.16.2", VersionInfoUtils.getVersion());
+        assertEquals("2.16.3", VersionInfoUtils.getVersion());
     }
 
     @Test
