1ded7e547dd702a8ea2a1ed201705a0f13ec2211
Roshan Kumar <roskumr@amazon.com>
2019-11-22 11:04:18 -0800

    Revert "[bump version 2.16.4] (#1321)"
    This reverts commit 6c7dd897d0b13efba301ce77238df1d26ca943e2.

--- aws-android-sdk-core/src/main/java/com/amazonaws/util/VersionInfoUtils.java
+++ aws-android-sdk-core/src/main/java/com/amazonaws/util/VersionInfoUtils.java
@@ -26,7 +26,7 @@ public class VersionInfoUtils {
     private static final int DEFAULT_STRING_LENGTH = 128;
 
     /** SDK version info */
-    private static volatile String version = "2.16.4";
+    private static volatile String version = "2.16.3";
                                                                 // changed build
                                                                 // logic
 
