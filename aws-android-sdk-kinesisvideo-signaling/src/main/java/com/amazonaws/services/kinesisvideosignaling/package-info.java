48212d64e58fac1e11369c447ff54782ed11974e
Raphael Kim <52714340+raphkim@users.noreply.github.com>
2019-12-09 16:13:19 -0800

    Support Kinesis Video Signaling (#1359)
    * Support Kinesis Video Signaling
    
    * Update release scripts and add test module

---
+++ aws-android-sdk-kinesisvideo-signaling/src/main/java/com/amazonaws/services/kinesisvideosignaling/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <p>Kinesis Video Streams Signaling Service is a intermediate service that establishes a communication channel for discovering peers, transmitting offers and answers in order to establish peer-to-peer connection in webRTC technology.</p>
+ */
+
+package com.amazonaws.services.kinesisvideosignaling;
+
