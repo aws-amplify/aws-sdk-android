ca7632f7025f60c9a1bb4b3830e298fdfb4b923d
Roshan Kumar <roskumr@amazon.com>
2019-12-19 15:04:13 -0800

    Update confirmSignIn api to accept client meta data  (#1379)

--- aws-android-sdk-mobile-client/src/androidTest/java/com/amazonaws/mobile/client/AWSMobileClientCustomAuthTest.java
+++ aws-android-sdk-mobile-client/src/androidTest/java/com/amazonaws/mobile/client/AWSMobileClientCustomAuthTest.java
@@ -214,8 +214,10 @@ public class AWSMobileClientCustomAuthTest extends AWSMobileClientTestBase {
 
     private void confirmSignIn() {
         final Map<String, String> res = new HashMap<String, String>();
-        res.put(CognitoServiceConstants.CHLG_RESP_ANSWER, "1133");
-        auth.confirmSignIn(res, new Callback<SignInResult>() {
+        final Map<String, String> clientMetaData = new HashMap<String, String>();
+        clientMetaData.put("challengeResponseAnswer", "1134");
+        res.put(CognitoServiceConstants.CHLG_RESP_ANSWER, "1134");
+        auth.confirmSignIn(res, clientMetaData, new Callback<SignInResult>() {
             @Override
             public void onResult(final SignInResult signInResult) {
                 runOnUiThread(new Runnable() {
