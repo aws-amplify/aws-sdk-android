3f6932db2caa270e6cbb6365e996356aede4cd7f
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2019-12-29 08:02:47 -0800

    [aws-android-sdk-iot] Update models to latest

--- aws-android-sdk-iot/src/main/java/com/amazonaws/services/iot/model/transform/CreateOTAUpdateRequestMarshaller.java
+++ aws-android-sdk-iot/src/main/java/com/amazonaws/services/iot/model/transform/CreateOTAUpdateRequestMarshaller.java
@@ -77,6 +77,17 @@ public class CreateOTAUpdateRequestMarshaller implements
                 }
                 jsonWriter.endArray();
             }
+            if (createOTAUpdateRequest.getProtocols() != null) {
+                java.util.List<String> protocols = createOTAUpdateRequest.getProtocols();
+                jsonWriter.name("protocols");
+                jsonWriter.beginArray();
+                for (String protocolsItem : protocols) {
+                    if (protocolsItem != null) {
+                        jsonWriter.value(protocolsItem);
+                    }
+                }
+                jsonWriter.endArray();
+            }
             if (createOTAUpdateRequest.getTargetSelection() != null) {
                 String targetSelection = createOTAUpdateRequest.getTargetSelection();
                 jsonWriter.name("targetSelection");
@@ -88,6 +99,13 @@ public class CreateOTAUpdateRequestMarshaller implements
                 jsonWriter.name("awsJobExecutionsRolloutConfig");
                 AwsJobExecutionsRolloutConfigJsonMarshaller.getInstance().marshall(
                         awsJobExecutionsRolloutConfig, jsonWriter);
+            }
+            if (createOTAUpdateRequest.getAwsJobPresignedUrlConfig() != null) {
+                AwsJobPresignedUrlConfig awsJobPresignedUrlConfig = createOTAUpdateRequest
+                        .getAwsJobPresignedUrlConfig();
+                jsonWriter.name("awsJobPresignedUrlConfig");
+                AwsJobPresignedUrlConfigJsonMarshaller.getInstance().marshall(
+                        awsJobPresignedUrlConfig, jsonWriter);
             }
             if (createOTAUpdateRequest.getFiles() != null) {
                 java.util.List<OTAUpdateFile> files = createOTAUpdateRequest.getFiles();
