39131ccf3512fc0f35b79243746f0e9056201df2
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2020-01-06 13:33:12 -0800

    [aws-android-sdk-kinesisvideo-signaling] Update models to latest (#1414)

--- aws-android-sdk-kinesisvideo-signaling/src/main/java/com/amazonaws/services/kinesisvideosignaling/model/GetIceServerConfigResult.java
+++ aws-android-sdk-kinesisvideo-signaling/src/main/java/com/amazonaws/services/kinesisvideosignaling/model/GetIceServerConfigResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -104,7 +104,7 @@ public class GetIceServerConfigResult implements Serializable {
      * debugging.
      *
      * @return A string representation of this object.
-     * @see Object#toString()
+     * @see java.lang.Object#toString()
      */
     @Override
     public String toString() {
