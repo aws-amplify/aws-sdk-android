e93b52794e0f1a68e01277542d41f42eab5970f1
Raphael Kim <52714340+raphkim@users.noreply.github.com>
2020-03-30 09:34:39 -0700

    Add user attributes to endpoint profile user (#1535)
    * Add user attributes to endpoint profile user
    
    * Null check in toJSONObject()
    
    * Update changelog

--- aws-android-sdk-pinpoint/src/main/java/com/amazonaws/mobileconnectors/pinpoint/targeting/endpointProfile/EndpointProfileUser.java
+++ aws-android-sdk-pinpoint/src/main/java/com/amazonaws/mobileconnectors/pinpoint/targeting/endpointProfile/EndpointProfileUser.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2016-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -15,16 +15,27 @@
 
 package com.amazonaws.mobileconnectors.pinpoint.targeting.endpointProfile;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.amazonaws.logging.Log;
+import com.amazonaws.logging.LogFactory;
 import com.amazonaws.mobileconnectors.pinpoint.internal.core.util.JSONBuilder;
 import com.amazonaws.mobileconnectors.pinpoint.internal.core.util.JSONSerializable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * An endpoint profile user
  */
 public class EndpointProfileUser implements JSONSerializable {
+    private static final Log log = LogFactory.getLog(EndpointProfileUser.class);
 
     private String userId;
+    private Map<String, List<String>> userAttributes;
 
     public String getUserId() {
         return this.userId;
@@ -34,10 +45,45 @@ public class EndpointProfileUser implements JSONSerializable {
         this.userId = userId;
     }
 
+    public Map<String, List<String>> getUserAttributes() {
+        return this.userAttributes;
+    }
+
+    public void setUserAttributes(Map<String, List<String>> userAttributes) {
+        this.userAttributes = userAttributes;
+    }
+
+    public EndpointProfileUser addUserAttribute(String key, List<String> value) {
+        if (this.userAttributes == null) {
+            this.userAttributes = new ConcurrentHashMap<>();
+        }
+        this.userAttributes.put(key, value);
+        return this;
+    }
+
     @Override
     public JSONObject toJSONObject() {
         final JSONBuilder builder = new JSONBuilder(null);
         builder.withAttribute("UserId", getUserId());
+
+        if (getUserAttributes() != null) {
+            final JSONObject attributesJson = new JSONObject();
+            for (final Map.Entry<String, List<String>> entry : getUserAttributes().entrySet()) {
+                try {
+                    final JSONArray array = new JSONArray(entry.getValue());
+                    attributesJson.put(entry.getKey(), array);
+                } catch (final JSONException e) {
+                    // Do not log e due to potentially sensitive information
+                    log.warn("Error serializing user attributes.");
+                }
+            }
+
+            // If there are any attributes put then add the attributes to the structure
+            if (attributesJson.length() > 0) {
+                builder.withAttribute("UserAttributes", attributesJson);
+            }
+        }
+
         return builder.toJSONObject();
     }
 }
