fa4e5f2b70de434b6f9be621643822fe06756a79
Jameson Williams <jhwill@amazon.com>
2020-06-24 06:31:50 -0700

    chore: remove unused test modules (#1925)
    Instrumentation tests are removed from the following modules. For any
    `-test` module, the module itself is removed.
    
      1. `aws-android-sdk-autoscaling-test`
      2. `aws-android-sdk-cloudwatch-test`
      3. `aws-android-sdk-cognitoauth`
      4. `aws-android-sdk-comprehend-test`
      5. `aws-android-sdk-connect-test`
      6. `aws-android-sdk-connectparticipant-test`
      7. `aws-android-sdk-ddb-mapper-test`
      8. `aws-android-sdk-ddb-test`
      9. `aws-android-sdk-elb-test`
      10. `aws-android-sdk-kinesis-test`
      11. `aws-android-sdk-kinesisvideo`
      12. `aws-android-sdk-lambda-test`
      13. `aws-android-sdk-machinelearning-test`
      14. `aws-android-sdk-polly-test`
      15. `aws-android-sdk-rekognition-test`
      16. `aws-android-sdk-sdb-test`
      17. `aws-android-sdk-ses-test`
      18. `aws-android-sdk-sns-test`
      19. `aws-android-sdk-sqs-test`
      20. `aws-android-sdk-textract-test`
      21. `aws-android-sdk-transcribe-test`
      22. `aws-android-sdk-translate-test`
    
    Unmodified instrumentation tests still exist in the following modules:
    
      1. `./aws-android-sdk-apigateway-test`
      2. `./aws-android-sdk-s3-test`
      3. `./aws-android-sdk-mobile-client`
      4. `./aws-android-sdk-core-test`
      5. `./aws-android-sdk-pinpoint-test`
      6. `./aws-android-sdk-iot`
      7. `./aws-android-sdk-cognitoidentityprovider-test`

--- aws-android-sdk-textract-test/src/main/java/com/amazonaws/services/textract/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>Amazon Textract detects and analyzes text in documents and converts it into machine-readable text. This is the API reference documentation for Amazon Textract.</p>
- */
-
-package com.amazonaws.services.textract;
-
