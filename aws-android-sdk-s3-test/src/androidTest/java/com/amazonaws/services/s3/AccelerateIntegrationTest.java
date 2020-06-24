06440c4bbbdd2b46763273f5d624c23ed4508408
John Pignata <john@pignata.com>
2020-06-23 20:00:30 -0400

    test(aws-android-sdk-s3-test): Fix flakey test missing a propagation delay
    testPutAndGetObjectWithAccelerateClient was failing intermittently as it
    didn't have the 2.5 second sleep present in the other tests. This sleep
    gives S3 time to propagate the bucket acceleration configuration change.
    
    This moves that logic into a method that will check up to 5 times every
    50 milliseconds for the change to make it more robust and save time in
    the test run.

--- aws-android-sdk-s3-test/src/androidTest/java/com/amazonaws/services/s3/AccelerateIntegrationTest.java
+++ aws-android-sdk-s3-test/src/androidTest/java/com/amazonaws/services/s3/AccelerateIntegrationTest.java
@@ -32,21 +32,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Date;
 
 public class AccelerateIntegrationTest extends S3IntegrationTestBase {
-
-    /** The name of the bucket these tests will create, test on and delete */
     private static final String BUCKET = "android-sdk-acceleration-integration-test-" + System.currentTimeMillis();
-
-    /** The key of the object these tests will create, test on and delete */
+    private static final int CONFIGURATION_PROPAGATION_WAIT_ATTEMPTS = 10;
+    private static final int CONFIGURATION_PROPAGATION_WAIT_TIME = 50;
     private static final String KEY = "key";
+
     private static AmazonS3Client accelerateS3;
 
     @Before
     public void setupBucket() throws Exception {
         s3.createBucket(BUCKET);
         S3IntegrationTestBase.waitForBucketCreation(BUCKET);
+
         accelerateS3 = new AmazonS3Client(credentials, Region.getRegion(Regions.US_WEST_1));
         accelerateS3.setS3ClientOptions(
                 S3ClientOptions.builder().setAccelerateModeEnabled(true).build());
@@ -71,34 +70,18 @@ public class AccelerateIntegrationTest extends S3IntegrationTestBase {
 
     @Test
     public void testGetAndSetAcceleration() throws Exception {
-        s3.setBucketAccelerateConfiguration(BUCKET,
-                new BucketAccelerateConfiguration(BucketAccelerateStatus.Enabled));
-        Thread.sleep(2500);
+        setBucketAccelerationAndWait(BUCKET, BucketAccelerateStatus.Enabled);
         assertEquals(BucketAccelerateStatus.Enabled.toString(),
                 s3.getBucketAccelerateConfiguration(BUCKET).getStatus());
 
-        s3.setBucketAccelerateConfiguration(BUCKET,
-                new BucketAccelerateConfiguration(BucketAccelerateStatus.Suspended));
-        Thread.sleep(2500);
+        setBucketAccelerationAndWait(BUCKET, BucketAccelerateStatus.Suspended);
         assertEquals(BucketAccelerateStatus.Suspended.toString(),
                 s3.getBucketAccelerateConfiguration(BUCKET).getStatus());
     }
 
-    @Test
-    public void testSetAccelerationWithAccelerateClient() throws Exception {
-        accelerateS3.setBucketAccelerateConfiguration(BUCKET,
-                new BucketAccelerateConfiguration(BucketAccelerateStatus.Enabled));
-        Thread.sleep(2500);
-        assertEquals(BucketAccelerateStatus.Enabled.toString(),
-                accelerateS3.getBucketAccelerateConfiguration(BUCKET).getStatus());
-    }
-
     @Test(expected = AmazonS3Exception.class)
     public void testAccessWithAccelerateClient() throws Exception {
-        s3.setBucketAccelerateConfiguration(BUCKET,
-                new BucketAccelerateConfiguration(BucketAccelerateStatus.Suspended));
-        // it takes time to propagate the configuration
-        Thread.sleep(2500);
+        setBucketAccelerationAndWait(BUCKET, BucketAccelerateStatus.Suspended);
 
         try {
             accelerateS3.getBucketAccelerateConfiguration(BUCKET);
@@ -112,8 +95,8 @@ public class AccelerateIntegrationTest extends S3IntegrationTestBase {
 
     @Test
     public void testPutAndGetObjectWithAccelerateClient() throws Exception {
-        accelerateS3.setBucketAccelerateConfiguration(BUCKET,
-                new BucketAccelerateConfiguration(BucketAccelerateStatus.Enabled));
+        setBucketAccelerationAndWait(BUCKET, BucketAccelerateStatus.Enabled);
+
         long contentLength = 128 * 1024;
         File file = getRandomTempFile(KEY, contentLength);
         accelerateS3.putObject(BUCKET, KEY, file);
@@ -124,5 +107,35 @@ public class AccelerateIntegrationTest extends S3IntegrationTestBase {
         assertNotNull(obj);
         assertFileEqualsStream(file, obj.getObjectContent());
         obj.getObjectContent().close();
+    }
+
+    void setBucketAccelerationAndWait(String bucket, BucketAccelerateStatus status)
+            throws InterruptedException {
+        accelerateS3.setBucketAccelerateConfiguration(bucket,
+                new BucketAccelerateConfiguration(status));
+
+        for (int i = 0; i < CONFIGURATION_PROPAGATION_WAIT_ATTEMPTS; i++) {
+            if (getBucketAccelerationStatus(bucket) == status) {
+                return;
+            }
+
+            Thread.sleep(CONFIGURATION_PROPAGATION_WAIT_TIME);
+        }
+
+        throw new IllegalStateException(
+                "Could not configure acceleration to " + status + " on " + bucket);
+    }
+
+    BucketAccelerateStatus getBucketAccelerationStatus(String bucket) {
+        try {
+            BucketAccelerateConfiguration config =
+                    accelerateS3.getBucketAccelerateConfiguration(bucket);
+
+            return BucketAccelerateStatus.fromValue(config.getStatus());
+        } catch (AmazonS3Exception e) {
+            // Apparently if acceleration is disabled, the service returns an error for a
+            // configuration query. If we receive any exception return Suspended.
+            return BucketAccelerateStatus.Suspended;
+        }
     }
 }
