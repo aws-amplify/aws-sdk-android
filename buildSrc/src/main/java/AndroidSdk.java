a812b42ecd0644ab122c67e5b64fcaaaf9b41f12
Artur Dryomov <artur.dryomov@gmail.com>
2020-07-10 21:39:31 +0300

    Change indentation

--- buildSrc/src/main/java/AndroidSdk.java
+++ buildSrc/src/main/java/AndroidSdk.java
@@ -1,6 +1,6 @@
 class AndroidSdk {
-		public static final int COMPILE = 28;
-		public static final int MINIMUM = 15;
-		public static final int MINIMUM_TEST = 18; // UI Automator
-		public static final int TARGET = 28;
+    public static final int COMPILE = 28;
+    public static final int MINIMUM = 15;
+    public static final int MINIMUM_TEST = 18; // UI Automator
+    public static final int TARGET = 28;
 }
