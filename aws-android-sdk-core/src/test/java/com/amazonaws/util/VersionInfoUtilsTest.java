9d462ede8fd3e272af5f9b2aadefae9eab8b0fb1
awsmobilesdk <circleci@ea908549885d>
2020-07-31 21:25:26 +0000

    [bump version 2.17.0]

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.16.13", VersionInfoUtils.getVersion());
+        assertEquals("2.17.0", VersionInfoUtils.getVersion());
     }
 
     @Test
