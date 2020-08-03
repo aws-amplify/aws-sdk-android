af1bb2c0fe68f56ba5f15d0c9d88de2ce236d92b
Jameson Williams <jhwill@amazon.com>
2020-08-03 07:41:58 -0600

    fix: remove deprecated wifi management from tests (#2044)
    The use of `WifiManager`'s `setWifiEnabled(...)` was originally added to
    this code base with the intention of toggling on/off connectivity, to
    test offline scenarios. From the beginning, this usage was flawed. The
    Android emulator will happily continue to serve traffic through a fake
    cellular connection. So, unless you pre-arrange a device to not have any
    other source of connetivity than WiFi, the call to `setWifiEnabled(...)`
    is useless.
    
    Worse, as of API 29, this method is officially deprecated. Oh well.
    
    As a replacement, use the UIAutomator-based `InternetConnectivity`
    utility that had recently been added to this codebase under
    `-testutils`.  This utility works by clicking the airplane mode button
    in the device's settings drop-down.
    
    This change removes the use of a deprecated API, and also improves the
    correctness of the offline tests. One that had been `@Ignore`d is
    re-enabled.
    
    This commit includes assorted code quality fixes in the effected files,
    as well.

--- aws-android-sdk-pinpoint-test/src/androidTest/java/com/amazonaws/mobileconnectors/pinpoint/analytics/EndpointProfileIntegrationTest.java
+++ aws-android-sdk-pinpoint-test/src/androidTest/java/com/amazonaws/mobileconnectors/pinpoint/analytics/EndpointProfileIntegrationTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2019-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
@@ -15,11 +15,6 @@
 
 package com.amazonaws.mobileconnectors.pinpoint.analytics;
 
-import android.content.Context;
-import android.net.wifi.WifiManager;
-
-import androidx.test.core.app.ApplicationProvider;
-
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointConfiguration;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointManager;
@@ -30,14 +25,15 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.pinpoint.model.ChannelType;
 import com.amazonaws.testutils.AWSTestBase;
 
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 
-import static junit.framework.TestCase.assertTrue;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static com.amazonaws.testutils.util.InternetConnectivity.goOnline;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -48,53 +44,31 @@ import static org.junit.Assert.assertNull;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class EndpointProfileIntegrationTest extends AWSTestBase {
-
-    private static Context appContext;
-
     private PinpointManager pinpointManager;
-    private PinpointConfiguration pinpointConfiguration;
     private CognitoCachingCredentialsProvider credentialsProvider;
-    private WifiManager wifiManager;
-
-    private String appId;
-    private Regions regions;
-
-    private static String TAG = EndpointProfileIntegrationTest.class.getSimpleName();
 
     @Before
     public void setUp() throws Exception {
-        final String identityPoolId = getPackageConfigure("pinpoint")
-                .getString("identity_pool_id");
+        JSONObject testConfig = getPackageConfigure("pinpoint");
+        String identityPoolId = testConfig.getString("identity_pool_id");
+        String appId = testConfig.getString("AppId");
+        Regions regions = Regions.fromName(testConfig.getString("Region"));
 
-        appId = getPackageConfigure("pinpoint")
-                .getString("AppId");
-        regions = Regions.fromName(getPackageConfigure("pinpoint")
-                .getString("Region"));
+        getApplicationContext().deleteDatabase("awspinpoint.db");
+        goOnline();
 
-        appContext = ApplicationProvider.getApplicationContext();
-        appContext.deleteDatabase("awspinpoint.db");
-
-        wifiManager = (WifiManager) ApplicationProvider.getApplicationContext()
-                .getSystemService(Context.WIFI_SERVICE);
-        assertTrue(wifiManager.setWifiEnabled(true));
-
-        credentialsProvider = new CognitoCachingCredentialsProvider(
-                appContext,
-                identityPoolId,
-                regions);
-        pinpointConfiguration = new PinpointConfiguration(appContext,
-                appId,
-                regions,
-                ChannelType.GCM,
-                credentialsProvider);
+        credentialsProvider =
+            new CognitoCachingCredentialsProvider(getApplicationContext(), identityPoolId, regions);
+        PinpointConfiguration pinpointConfiguration =
+            new PinpointConfiguration(getApplicationContext(), appId, regions, ChannelType.GCM, credentialsProvider);
         pinpointManager = new PinpointManager(pinpointConfiguration);
     }
 
     @After
     public void tearDown() {
-        assertTrue(wifiManager.setWifiEnabled(true));
+        goOnline();
         pinpointManager.getAnalyticsClient().closeDB();
-        appContext.deleteDatabase("awspinpoint.db");
+        getApplicationContext().deleteDatabase("awspinpoint.db");
     }
 
     @Test
@@ -116,18 +90,18 @@ public class EndpointProfileIntegrationTest extends AWSTestBase {
         targetingClient.updateEndpointProfile();
         endpointProfile = targetingClient.currentEndpoint();
         assertNotNull(endpointProfile);
-        assertEquals(credentialsProvider.getIdentityId(),
-                endpointProfile.getUser().getUserId());
+        assertEquals(credentialsProvider.getIdentityId(), endpointProfile.getUser().getUserId());
         assertNotNull(endpointProfile.getUser().getUserAttributes());
-        assertEquals(Collections.singletonMap("user-key", Collections.singletonList("user-value")),
-                endpointProfile.getUser().getUserAttributes());
+        assertEquals(
+            Collections.singletonMap("user-key", Collections.singletonList("user-value")),
+            endpointProfile.getUser().getUserAttributes()
+        );
 
-        endpointProfile.addAttribute("key", Arrays.asList("value"));
+        endpointProfile.addAttribute("key", Collections.singletonList("value"));
         targetingClient.updateEndpointProfile();
         endpointProfile = targetingClient.currentEndpoint();
         assertNotNull(endpointProfile);
-        assertEquals(credentialsProvider.getIdentityId(),
-                endpointProfile.getUser().getUserId());
-        assertEquals("value", endpointProfile.getAllAttributes().get("key").get(0));
+        assertEquals(credentialsProvider.getIdentityId(), endpointProfile.getUser().getUserId());
+        assertEquals(Collections.singletonList("value"), endpointProfile.getAllAttributes().get("key"));
     }
 }
