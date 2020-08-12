d881724530126ac41250126247c146cadf330ec3
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2020-08-12 18:09:16 +0000

    feat(aws-android-sdk-comprehend): update models to latest

--- aws-android-sdk-comprehend/src/main/java/com/amazonaws/services/comprehend/model/UnsupportedLanguageException.java
+++ aws-android-sdk-comprehend/src/main/java/com/amazonaws/services/comprehend/model/UnsupportedLanguageException.java
@@ -21,9 +21,10 @@ import com.amazonaws.AmazonServiceException;
  * <p>
  * Amazon Comprehend can't process the language of the input text. For all
  * custom entity recognition APIs (such as <code>CreateEntityRecognizer</code>),
- * only English is accepted. For most other APIs, such as those for Custom
- * Classification, Amazon Comprehend accepts text in all supported languages.
- * For a list of supported languages, see <a>supported-languages</a>.
+ * only English, Spanish, French, Italian, German, or Portuguese are accepted.
+ * For most other APIs, such as those for Custom Classification, Amazon
+ * Comprehend accepts text in all supported languages. For a list of supported
+ * languages, see <a>supported-languages</a>.
  * </p>
  */
 public class UnsupportedLanguageException extends AmazonServiceException {
