43751ead2d72c71e6da22ee4cccfad359f88d85a
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2020-10-15 18:07:52 +0000

    feat(aws-android-sdk-rekognition): update models to latest

--- aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/AmazonRekognition.java
+++ aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/AmazonRekognition.java
@@ -948,6 +948,102 @@ public interface AmazonRekognition {
 
     /**
      * <p>
+     * Detects Personal Protective Equipment (PPE) worn by people detected in an
+     * image. Amazon Rekognition can detect the following types of PPE.
+     * </p>
+     * <ul>
+     * <li>
+     * <p>
+     * Face cover
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * Hand cover
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * Head cover
+     * </p>
+     * </li>
+     * </ul>
+     * <p>
+     * You pass the input image as base64-encoded image bytes or as a reference
+     * to an image in an Amazon S3 bucket. The image must be either a PNG or JPG
+     * formatted file.
+     * </p>
+     * <p>
+     * <code>DetectProtectiveEquipment</code> detects PPE worn by up to 15
+     * persons detected in an image.
+     * </p>
+     * <p>
+     * For each person detected in the image the API returns an array of body
+     * parts (face, head, left-hand, right-hand). For each body part, an array
+     * of detected items of PPE is returned, including an indicator of whether
+     * or not the PPE covers the body part. The API returns the confidence it
+     * has in each detection (person, PPE, body part and body part coverage). It
+     * also returns a bounding box (<a>BoundingBox</a>) for each detected person
+     * and each detected item of PPE.
+     * </p>
+     * <p>
+     * You can optionally request a summary of detected PPE items with the
+     * <code>SummarizationAttributes</code> input parameter. The summary
+     * provides the following information.
+     * </p>
+     * <ul>
+     * <li>
+     * <p>
+     * The persons detected as wearing all of the types of PPE that you specify.
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * The persons detected as not wearing all of the types PPE that you
+     * specify.
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * The persons detected where PPE adornment could not be determined.
+     * </p>
+     * </li>
+     * </ul>
+     * <p>
+     * This is a stateless API operation. That is, the operation does not
+     * persist any data.
+     * </p>
+     * <p>
+     * This operation requires permissions to perform the
+     * <code>rekognition:DetectProtectiveEquipment</code> action.
+     * </p>
+     * 
+     * @param detectProtectiveEquipmentRequest
+     * @return detectProtectiveEquipmentResult The response from the
+     *         DetectProtectiveEquipment service method, as returned by Amazon
+     *         Rekognition.
+     * @throws InvalidS3ObjectException
+     * @throws InvalidParameterException
+     * @throws ImageTooLargeException
+     * @throws AccessDeniedException
+     * @throws InternalServerErrorException
+     * @throws ThrottlingException
+     * @throws ProvisionedThroughputExceededException
+     * @throws InvalidImageFormatException
+     * @throws AmazonClientException If any internal errors are encountered
+     *             inside the client while attempting to make the request or
+     *             handle the response. For example if a network connection is
+     *             not available.
+     * @throws AmazonServiceException If an error response is returned by Amazon
+     *             Rekognition indicating either a problem with the data in the
+     *             request, or a server side issue.
+     */
+    DetectProtectiveEquipmentResult detectProtectiveEquipment(
+            DetectProtectiveEquipmentRequest detectProtectiveEquipmentRequest)
+            throws AmazonClientException, AmazonServiceException;
+
+    /**
+     * <p>
      * Detects text in the input image and converts it into machine-readable
      * text.
      * </p>
@@ -1777,6 +1873,7 @@ public interface AmazonRekognition {
      * @throws ProvisionedThroughputExceededException
      * @throws ResourceNotFoundException
      * @throws InvalidImageFormatException
+     * @throws ServiceQuotaExceededException
      * @throws AmazonClientException If any internal errors are encountered
      *             inside the client while attempting to make the request or
      *             handle the response. For example if a network connection is
---
+++ aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/model/ProtectiveEquipmentBodyPart.java
@@ -0,0 +1,360 @@
+/*
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazonaws.services.rekognition.model;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * Information about a body part detected by <a>DetectProtectiveEquipment</a>
+ * that contains PPE. An array of <code>ProtectiveEquipmentBodyPart</code>
+ * objects is returned for each person detected by
+ * <code>DetectProtectiveEquipment</code>.
+ * </p>
+ */
+public class ProtectiveEquipmentBodyPart implements Serializable {
+    /**
+     * <p>
+     * The detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Allowed Values: </b>FACE, HEAD, LEFT_HAND, RIGHT_HAND
+     */
+    private String name;
+
+    /**
+     * <p>
+     * The confidence that Amazon Rekognition has in the detection accuracy of
+     * the detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>0 - 100<br/>
+     */
+    private Float confidence;
+
+    /**
+     * <p>
+     * An array of Personal Protective Equipment items detected around a body
+     * part.
+     * </p>
+     */
+    private java.util.List<EquipmentDetection> equipmentDetections;
+
+    /**
+     * <p>
+     * The detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Allowed Values: </b>FACE, HEAD, LEFT_HAND, RIGHT_HAND
+     *
+     * @return <p>
+     *         The detected body part.
+     *         </p>
+     * @see BodyPart
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * <p>
+     * The detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Allowed Values: </b>FACE, HEAD, LEFT_HAND, RIGHT_HAND
+     *
+     * @param name <p>
+     *            The detected body part.
+     *            </p>
+     * @see BodyPart
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * <p>
+     * The detected body part.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Allowed Values: </b>FACE, HEAD, LEFT_HAND, RIGHT_HAND
+     *
+     * @param name <p>
+     *            The detected body part.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     * @see BodyPart
+     */
+    public ProtectiveEquipmentBodyPart withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * <p>
+     * The detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Allowed Values: </b>FACE, HEAD, LEFT_HAND, RIGHT_HAND
+     *
+     * @param name <p>
+     *            The detected body part.
+     *            </p>
+     * @see BodyPart
+     */
+    public void setName(BodyPart name) {
+        this.name = name.toString();
+    }
+
+    /**
+     * <p>
+     * The detected body part.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Allowed Values: </b>FACE, HEAD, LEFT_HAND, RIGHT_HAND
+     *
+     * @param name <p>
+     *            The detected body part.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     * @see BodyPart
+     */
+    public ProtectiveEquipmentBodyPart withName(BodyPart name) {
+        this.name = name.toString();
+        return this;
+    }
+
+    /**
+     * <p>
+     * The confidence that Amazon Rekognition has in the detection accuracy of
+     * the detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>0 - 100<br/>
+     *
+     * @return <p>
+     *         The confidence that Amazon Rekognition has in the detection
+     *         accuracy of the detected body part.
+     *         </p>
+     */
+    public Float getConfidence() {
+        return confidence;
+    }
+
+    /**
+     * <p>
+     * The confidence that Amazon Rekognition has in the detection accuracy of
+     * the detected body part.
+     * </p>
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>0 - 100<br/>
+     *
+     * @param confidence <p>
+     *            The confidence that Amazon Rekognition has in the detection
+     *            accuracy of the detected body part.
+     *            </p>
+     */
+    public void setConfidence(Float confidence) {
+        this.confidence = confidence;
+    }
+
+    /**
+     * <p>
+     * The confidence that Amazon Rekognition has in the detection accuracy of
+     * the detected body part.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     * <p>
+     * <b>Constraints:</b><br/>
+     * <b>Length: </b>0 - 100<br/>
+     *
+     * @param confidence <p>
+     *            The confidence that Amazon Rekognition has in the detection
+     *            accuracy of the detected body part.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     */
+    public ProtectiveEquipmentBodyPart withConfidence(Float confidence) {
+        this.confidence = confidence;
+        return this;
+    }
+
+    /**
+     * <p>
+     * An array of Personal Protective Equipment items detected around a body
+     * part.
+     * </p>
+     *
+     * @return <p>
+     *         An array of Personal Protective Equipment items detected around a
+     *         body part.
+     *         </p>
+     */
+    public java.util.List<EquipmentDetection> getEquipmentDetections() {
+        return equipmentDetections;
+    }
+
+    /**
+     * <p>
+     * An array of Personal Protective Equipment items detected around a body
+     * part.
+     * </p>
+     *
+     * @param equipmentDetections <p>
+     *            An array of Personal Protective Equipment items detected
+     *            around a body part.
+     *            </p>
+     */
+    public void setEquipmentDetections(java.util.Collection<EquipmentDetection> equipmentDetections) {
+        if (equipmentDetections == null) {
+            this.equipmentDetections = null;
+            return;
+        }
+
+        this.equipmentDetections = new java.util.ArrayList<EquipmentDetection>(equipmentDetections);
+    }
+
+    /**
+     * <p>
+     * An array of Personal Protective Equipment items detected around a body
+     * part.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     *
+     * @param equipmentDetections <p>
+     *            An array of Personal Protective Equipment items detected
+     *            around a body part.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     */
+    public ProtectiveEquipmentBodyPart withEquipmentDetections(
+            EquipmentDetection... equipmentDetections) {
+        if (getEquipmentDetections() == null) {
+            this.equipmentDetections = new java.util.ArrayList<EquipmentDetection>(
+                    equipmentDetections.length);
+        }
+        for (EquipmentDetection value : equipmentDetections) {
+            this.equipmentDetections.add(value);
+        }
+        return this;
+    }
+
+    /**
+     * <p>
+     * An array of Personal Protective Equipment items detected around a body
+     * part.
+     * </p>
+     * <p>
+     * Returns a reference to this object so that method calls can be chained
+     * together.
+     *
+     * @param equipmentDetections <p>
+     *            An array of Personal Protective Equipment items detected
+     *            around a body part.
+     *            </p>
+     * @return A reference to this updated object so that method calls can be
+     *         chained together.
+     */
+    public ProtectiveEquipmentBodyPart withEquipmentDetections(
+            java.util.Collection<EquipmentDetection> equipmentDetections) {
+        setEquipmentDetections(equipmentDetections);
+        return this;
+    }
+
+    /**
+     * Returns a string representation of this object; useful for testing and
+     * debugging.
+     *
+     * @return A string representation of this object.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        if (getName() != null)
+            sb.append("Name: " + getName() + ",");
+        if (getConfidence() != null)
+            sb.append("Confidence: " + getConfidence() + ",");
+        if (getEquipmentDetections() != null)
+            sb.append("EquipmentDetections: " + getEquipmentDetections());
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hashCode = 1;
+
+        hashCode = prime * hashCode + ((getName() == null) ? 0 : getName().hashCode());
+        hashCode = prime * hashCode + ((getConfidence() == null) ? 0 : getConfidence().hashCode());
+        hashCode = prime * hashCode
+                + ((getEquipmentDetections() == null) ? 0 : getEquipmentDetections().hashCode());
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+
+        if (obj instanceof ProtectiveEquipmentBodyPart == false)
+            return false;
+        ProtectiveEquipmentBodyPart other = (ProtectiveEquipmentBodyPart) obj;
+
+        if (other.getName() == null ^ this.getName() == null)
+            return false;
+        if (other.getName() != null && other.getName().equals(this.getName()) == false)
+            return false;
+        if (other.getConfidence() == null ^ this.getConfidence() == null)
+            return false;
+        if (other.getConfidence() != null
+                && other.getConfidence().equals(this.getConfidence()) == false)
+            return false;
+        if (other.getEquipmentDetections() == null ^ this.getEquipmentDetections() == null)
+            return false;
+        if (other.getEquipmentDetections() != null
+                && other.getEquipmentDetections().equals(this.getEquipmentDetections()) == false)
+            return false;
+        return true;
+    }
+}
