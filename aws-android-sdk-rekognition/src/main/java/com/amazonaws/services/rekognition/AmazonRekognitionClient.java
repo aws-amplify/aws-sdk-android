1d35978a5b669a4b2b856ec4a18223277eee5f06
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2020-10-15 18:07:52 +0000

    feat(aws-android-sdk-rekognition): update models to latest

--- aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/AmazonRekognitionClient.java
+++ aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/AmazonRekognitionClient.java
@@ -339,6 +339,7 @@ public class AmazonRekognitionClient extends AmazonWebServiceClient implements A
         jsonErrorUnmarshallers.add(new ResourceInUseExceptionUnmarshaller());
         jsonErrorUnmarshallers.add(new ResourceNotFoundExceptionUnmarshaller());
         jsonErrorUnmarshallers.add(new ResourceNotReadyExceptionUnmarshaller());
+        jsonErrorUnmarshallers.add(new ServiceQuotaExceededExceptionUnmarshaller());
         jsonErrorUnmarshallers.add(new ThrottlingExceptionUnmarshaller());
         jsonErrorUnmarshallers.add(new VideoTooLargeExceptionUnmarshaller());
         jsonErrorUnmarshallers.add(new JsonErrorUnmarshaller());
@@ -1702,6 +1703,129 @@ public class AmazonRekognitionClient extends AmazonWebServiceClient implements A
 
     /**
      * <p>
+     * Detects Personal Protective Equipment (PPE) worn by people detected in an
+     * image. Amazon Rekognition can detect the following types of PPE.
+     * </p>
+     * <ul>
+     * <li>
+     * <p>
+     * Face cover
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * Hand cover
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * Head cover
+     * </p>
+     * </li>
+     * </ul>
+     * <p>
+     * You pass the input image as base64-encoded image bytes or as a reference
+     * to an image in an Amazon S3 bucket. The image must be either a PNG or JPG
+     * formatted file.
+     * </p>
+     * <p>
+     * <code>DetectProtectiveEquipment</code> detects PPE worn by up to 15
+     * persons detected in an image.
+     * </p>
+     * <p>
+     * For each person detected in the image the API returns an array of body
+     * parts (face, head, left-hand, right-hand). For each body part, an array
+     * of detected items of PPE is returned, including an indicator of whether
+     * or not the PPE covers the body part. The API returns the confidence it
+     * has in each detection (person, PPE, body part and body part coverage). It
+     * also returns a bounding box (<a>BoundingBox</a>) for each detected person
+     * and each detected item of PPE.
+     * </p>
+     * <p>
+     * You can optionally request a summary of detected PPE items with the
+     * <code>SummarizationAttributes</code> input parameter. The summary
+     * provides the following information.
+     * </p>
+     * <ul>
+     * <li>
+     * <p>
+     * The persons detected as wearing all of the types of PPE that you specify.
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * The persons detected as not wearing all of the types PPE that you
+     * specify.
+     * </p>
+     * </li>
+     * <li>
+     * <p>
+     * The persons detected where PPE adornment could not be determined.
+     * </p>
+     * </li>
+     * </ul>
+     * <p>
+     * This is a stateless API operation. That is, the operation does not
+     * persist any data.
+     * </p>
+     * <p>
+     * This operation requires permissions to perform the
+     * <code>rekognition:DetectProtectiveEquipment</code> action.
+     * </p>
+     * 
+     * @param detectProtectiveEquipmentRequest
+     * @return detectProtectiveEquipmentResult The response from the
+     *         DetectProtectiveEquipment service method, as returned by Amazon
+     *         Rekognition.
+     * @throws InvalidS3ObjectException
+     * @throws InvalidParameterException
+     * @throws ImageTooLargeException
+     * @throws AccessDeniedException
+     * @throws InternalServerErrorException
+     * @throws ThrottlingException
+     * @throws ProvisionedThroughputExceededException
+     * @throws InvalidImageFormatException
+     * @throws AmazonClientException If any internal errors are encountered
+     *             inside the client while attempting to make the request or
+     *             handle the response. For example if a network connection is
+     *             not available.
+     * @throws AmazonServiceException If an error response is returned by Amazon
+     *             Rekognition indicating either a problem with the data in the
+     *             request, or a server side issue.
+     */
+    public DetectProtectiveEquipmentResult detectProtectiveEquipment(
+            DetectProtectiveEquipmentRequest detectProtectiveEquipmentRequest)
+            throws AmazonServiceException, AmazonClientException {
+        ExecutionContext executionContext = createExecutionContext(detectProtectiveEquipmentRequest);
+        AWSRequestMetrics awsRequestMetrics = executionContext.getAwsRequestMetrics();
+        awsRequestMetrics.startEvent(Field.ClientExecuteTime);
+        Request<DetectProtectiveEquipmentRequest> request = null;
+        Response<DetectProtectiveEquipmentResult> response = null;
+        try {
+            awsRequestMetrics.startEvent(Field.RequestMarshallTime);
+            try {
+                request = new DetectProtectiveEquipmentRequestMarshaller()
+                        .marshall(detectProtectiveEquipmentRequest);
+                // Binds the request metrics to the current request.
+                request.setAWSRequestMetrics(awsRequestMetrics);
+            } finally {
+                awsRequestMetrics.endEvent(Field.RequestMarshallTime);
+            }
+            Unmarshaller<DetectProtectiveEquipmentResult, JsonUnmarshallerContext> unmarshaller = new DetectProtectiveEquipmentResultJsonUnmarshaller();
+            JsonResponseHandler<DetectProtectiveEquipmentResult> responseHandler = new JsonResponseHandler<DetectProtectiveEquipmentResult>(
+                    unmarshaller);
+
+            response = invoke(request, responseHandler, executionContext);
+
+            return response.getAwsResponse();
+        } finally {
+            awsRequestMetrics.endEvent(Field.ClientExecuteTime);
+            endClientExecution(awsRequestMetrics, request, response, LOGGING_AWS_REQUEST_METRIC);
+        }
+    }
+
+    /**
+     * <p>
      * Detects text in the input image and converts it into machine-readable
      * text.
      * </p>
@@ -2798,6 +2922,7 @@ public class AmazonRekognitionClient extends AmazonWebServiceClient implements A
      * @throws ProvisionedThroughputExceededException
      * @throws ResourceNotFoundException
      * @throws InvalidImageFormatException
+     * @throws ServiceQuotaExceededException
      * @throws AmazonClientException If any internal errors are encountered
      *             inside the client while attempting to make the request or
      *             handle the response. For example if a network connection is
