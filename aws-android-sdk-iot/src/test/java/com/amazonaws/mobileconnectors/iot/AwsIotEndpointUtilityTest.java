a9cebbdee5392d5fc2f3d01c965cca344dc2f991
John Pignata <john@pignata.com>
2021-01-09 16:08:06 -0500

    Improve AWS Region code extraction
    Previously this helper was looking for an AWS Region code within a
    static component of the endpoint. AWS IoT Device Advisor introduces a
    new format for test endpoints. Rather than adding to this logic, this
    change tokenizes the endpoint and looks for a matching AWS Region code.
    
    - Improve and tidy up the tests a bit

--- aws-android-sdk-iot/src/test/java/com/amazonaws/mobileconnectors/iot/AwsIotEndpointUtilityTest.java
+++ aws-android-sdk-iot/src/test/java/com/amazonaws/mobileconnectors/iot/AwsIotEndpointUtilityTest.java
@@ -8,70 +8,57 @@ import static org.junit.Assert.*;
 
 public class AwsIotEndpointUtilityTest {
 
+    public static final Region US_EAST_1 = Region.getRegion(Regions.US_EAST_1);
+    public static final Region CN_NORTH_1 = Region.getRegion(Regions.CN_NORTH_1);
+
     @Test
     public void testGetRegionFromCustomIotEndpoint() throws Exception {
         String testEndpoint = "ABCDEFG.iot.us-east-1.amazonaws.com";
-        assertEquals(Region.getRegion(Regions.US_EAST_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
+        assertEquals(US_EAST_1, AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
     }
 
     @Test
     public void testGetRegionFromCustomIotEndpointWithPort() throws Exception {
         String testEndpoint = "ABCDEFG.iot.us-east-1.amazonaws.com:443";
-        assertEquals(Region.getRegion(Regions.US_EAST_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testGetRegionFromCustomIotEndpointBadRegion1() throws Exception {
-        String testEndpoint = "ABCDEFG.iot.us-east.amazonaws.com";
-        assertEquals(Region.getRegion(Regions.US_EAST_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testGetRegionFromCustomIotEndpointBadRegion2() throws Exception {
-        String testEndpoint = "ABCDEFG.iot.us-east-9.amazonaws.com";
-        assertEquals(Region.getRegion(Regions.US_EAST_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testGetRegionFromCustomIotEndpointBadRegion3() throws Exception {
-        String testEndpoint = "ABCDEFG.iot.us-central-1.amazonaws.com";
-        assertEquals(Region.getRegion(Regions.US_EAST_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
+        assertEquals(US_EAST_1, AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
     }
 
     @Test
     public void testGetRegionFromDataIotEndpointWithPort() throws Exception {
         String testEndpoint = "data.iot.us-east-1.amazonaws.com:443";
-        assertEquals(Region.getRegion(Regions.US_EAST_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testGetRegionFromCustomIotEndpointNoPrefix() throws Exception {
-        String testEndpoint = "iot.us-east-1.amazonaws.com";
-        AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testRegionFromBadEndpointMissingCN() throws Exception {
-        String testEndpoint = "random.ats.iot.cn-north-1.amazonaws.com";
-        AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint);
+        assertEquals(US_EAST_1, AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
     }
 
     @Test
     public void testRegionFromCNEndpoint() throws Exception {
         String testEndpoint = "random.iot.cn-north-1.amazonaws.com.cn";
-        assertEquals(Region.getRegion(Regions.CN_NORTH_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
+        assertEquals(CN_NORTH_1, AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
     }
 
     @Test
     public void testRegionFromCNATSEndpoint() throws Exception {
         String testEndpoint = "random.ats.iot.cn-north-1.amazonaws.com.cn";
-        assertEquals(Region.getRegion(Regions.CN_NORTH_1), AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
-        assertEquals("amazonaws.com.cn", AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint).getDomain());
+        Region region = AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint);
+
+        assertEquals(CN_NORTH_1, region);
+        assertEquals("amazonaws.com.cn", region.getDomain());
     }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void testEndpointTooLongShouldThrowException() throws Exception {
-        String longEndpoint = "long.iot.cn-north-1.amazonaws.com.cn.long";
-        AwsIotEndpointUtility.getRegionFromIotEndpoint(longEndpoint);
+    @Test
+    public void testRegionFromUppercasedEndpoint() throws Exception {
+        String testEndpoint = "ABCDEFG.IOT.US-EAST-1.AMAZONAWS.COM";
+        assertEquals(US_EAST_1, AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetRegionFromCustomIotEndpointUnknownRegion() throws Exception {
+        String testEndpoint = "ABCDEFG.iot.us-pretend-1.amazonaws.com";
+        AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetRegionFromUnrelatedString() throws Exception {
+        String testEndpoint = "Lorem ipsum dolor sit amet";
+        AwsIotEndpointUtility.getRegionFromIotEndpoint(testEndpoint);
     }
 }
