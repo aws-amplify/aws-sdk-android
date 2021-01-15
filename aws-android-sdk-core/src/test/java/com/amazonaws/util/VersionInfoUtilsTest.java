fc1a63d6678d45255e201cc4ccba3a0be62510ae
Raphael Kim <raphk@amazon.com>
2021-01-15 14:07:32 -0800

    chore(release): Release v2.22.0

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.21.0", VersionInfoUtils.getVersion());
+        assertEquals("2.22.0", VersionInfoUtils.getVersion());
     }
 
     @Test
