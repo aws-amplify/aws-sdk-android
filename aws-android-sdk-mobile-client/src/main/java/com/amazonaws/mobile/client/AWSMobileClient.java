bb0af6272039d892ea597662ac1a47bb596699ad
Raphael Kim <52714340+raphkim@users.noreply.github.com>
2021-03-25 17:16:50 -0700

    Revert "fix: honor authenticationFlowType from config (#2377)" (#2399)
    This reverts commit 73bb3e432aee9fe1ffa224958434df92a8ef8b78.

--- aws-android-sdk-mobile-client/src/main/java/com/amazonaws/mobile/client/AWSMobileClient.java
+++ aws-android-sdk-mobile-client/src/main/java/com/amazonaws/mobile/client/AWSMobileClient.java
@@ -1206,33 +1206,20 @@ public final class AWSMobileClient implements AWSCredentialsProvider {
                             @Override
                             public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String userId) {
                                 Log.d(TAG, "Sending password.");
-                                final HashMap<String, String> authParameters = new HashMap<>();
-                                // Check if the auth flow type setting is in the configuration.
-                                boolean authFlowTypeInConfig =
-                                    awsConfiguration.optJsonObject(AUTH_KEY) != null &&
-                                    awsConfiguration.optJsonObject(AUTH_KEY).has("authenticationFlowType");
-
                                 try {
-                                    String authFlowType = authFlowTypeInConfig ?
-                                        awsConfiguration.optJsonObject(AUTH_KEY).getString("authenticationFlowType") :
-                                        null;
-                                    // If there's a value in the config and it's CUSTOM_AUTH
-                                    if (authFlowTypeInConfig && "CUSTOM_AUTH".equals(authFlowType)) {
+                                    if (
+                                            awsConfiguration.optJsonObject(AUTH_KEY) != null &&
+                                            awsConfiguration.optJsonObject(AUTH_KEY).has("authenticationFlowType") &&
+                                            awsConfiguration.optJsonObject(AUTH_KEY).getString("authenticationFlowType").equals("CUSTOM_AUTH")
+                                    ) {
+                                        final HashMap<String, String> authParameters = new HashMap<String, String>();
                                         if (password != null) {
                                             authenticationContinuation.setAuthenticationDetails(new AuthenticationDetails(username, password, authParameters, validationData));
                                         } else {
                                             authenticationContinuation.setAuthenticationDetails(new AuthenticationDetails(username, authParameters, validationData));
                                         }
                                     } else {
-                                        // Otherwise, create the AuthenticationDetails instance using the constructor below
-                                        // which will default the auth flow to CHLG_TYPE_USER_PASSWORD_VERIFIER
-                                        AuthenticationDetails authenticationDetails = new AuthenticationDetails(username, password, validationData);
-                                        if (authFlowTypeInConfig) {
-                                            // If there's an auth flow type value in the config, use that value instead.
-                                            // The field names are very misleading.
-                                            authenticationDetails.setAuthenticationType(awsConfiguration.optJsonObject(AUTH_KEY).getString("authenticationFlowType"));
-                                        }
-                                        authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+                                        authenticationContinuation.setAuthenticationDetails(new AuthenticationDetails(username, password, validationData));
                                     }
                                 } catch (JSONException e) {
                                     e.printStackTrace();
