1cfa31d288379d6edc0ac0a2ac9928844f65f67d
Raphael Kim <raphk@amazon.com>
2021-04-13 10:56:37 -0700

    implement retry mechanism for upload part

--- aws-android-sdk-s3/src/main/java/com/amazonaws/mobileconnectors/s3/transferutility/UploadPartTask.java
+++ aws-android-sdk-s3/src/main/java/com/amazonaws/mobileconnectors/s3/transferutility/UploadPartTask.java
@@ -15,6 +15,7 @@
 
 package com.amazonaws.mobileconnectors.s3.transferutility;
 
+import com.amazonaws.AbortedException;
 import com.amazonaws.event.ProgressEvent;
 import com.amazonaws.event.ProgressListener;
 import com.amazonaws.services.s3.AmazonS3;
@@ -28,10 +29,10 @@ import java.util.concurrent.Callable;
 
 class UploadPartTask implements Callable<Boolean> {
     private static final Log LOGGER = LogFactory.getLog(UploadPartTask.class);
-
+    private static final int RETRY_COUNT = 3;
 
     private final UploadTask.UploadPartTaskMetadata uploadPartTaskMetadata;
-    private final UploadTask.UploadTaskProgressListener uploadTaskProgressListener;
+    private final UploadPartTaskProgressListener uploadPartTaskProgressListener;
     private final UploadPartRequest uploadPartRequest;
     private final AmazonS3 s3;
     private final TransferDBUtil dbUtil;
@@ -42,7 +43,7 @@ class UploadPartTask implements Callable<Boolean> {
                           AmazonS3 s3,
                           TransferDBUtil dbUtil) {
         this.uploadPartTaskMetadata = uploadPartTaskMetadata;
-        this.uploadTaskProgressListener = uploadTaskProgressListener;
+        this.uploadPartTaskProgressListener = new UploadPartTaskProgressListener(uploadTaskProgressListener);
         this.uploadPartRequest = uploadPartRequest;
         this.s3 = s3;
         this.dbUtil = dbUtil;
@@ -53,45 +54,69 @@ class UploadPartTask implements Callable<Boolean> {
      */
     @Override
     public Boolean call() throws Exception {
-        try {
-            uploadPartTaskMetadata.state = TransferState.IN_PROGRESS;
-            uploadPartRequest.setGeneralProgressListener(new UploadPartTaskProgressListener(uploadTaskProgressListener));
-            final UploadPartResult putPartResult = s3.uploadPart(uploadPartRequest);
-            uploadPartTaskMetadata.state = TransferState.PART_COMPLETED;
-            dbUtil.updateState(uploadPartRequest.getId(), TransferState.PART_COMPLETED);
-            dbUtil.updateETag(uploadPartRequest.getId(), putPartResult.getETag());
-            return true;
-        } catch (final Exception e) {
-            LOGGER.error("Upload part interrupted: " + e);
-            ProgressEvent resetEvent = new ProgressEvent(0);
-            resetEvent.setEventCode(ProgressEvent.RESET_EVENT_CODE);
-            uploadTaskProgressListener.progressChanged(new ProgressEvent(0));
-
-            // Check if network is not connected, set the state to WAITING_FOR_NETWORK.
+        uploadPartTaskMetadata.state = TransferState.IN_PROGRESS;
+        uploadPartRequest.setGeneralProgressListener(uploadPartTaskProgressListener);
+        UploadPartResult putPartResult = null;
+        int retried = 0;
+        while (retried < RETRY_COUNT) {
             try {
-                if (TransferNetworkLossHandler.getInstance() != null &&
-                    !TransferNetworkLossHandler.getInstance().isNetworkConnected()) {
-                    LOGGER.info("Thread: [" + Thread.currentThread().getId() + "]: Network wasn't available.");
-                    /*
-                     * Network connection is being interrupted. Moving the TransferState
-                     * to WAITING_FOR_NETWORK till the network availability resumes.
-                     */
-                    uploadPartTaskMetadata.state = TransferState.WAITING_FOR_NETWORK;
-                    dbUtil.updateState(uploadPartRequest.getId(), TransferState.WAITING_FOR_NETWORK);
-                    LOGGER.info("Network Connection Interrupted: " +
-                            "Moving the TransferState to WAITING_FOR_NETWORK");
-                    return false;
-                }
-            } catch (TransferUtilityException transferUtilityException) {
-                LOGGER.error("TransferUtilityException: [" + transferUtilityException + "]");
-            }
+                putPartResult = s3.uploadPart(uploadPartRequest);
+                break;
+            } catch (AbortedException e) {
+                // If request got aborted, operation was paused or canceled. do not retry.
+                LOGGER.error("Upload part aborted.");
+                resetProgress();
+                return false;
+            } catch (final Exception e) {
+                LOGGER.error("Unexpected error occurred: " + e);
+                resetProgress();
 
-            // In other cases, set the transfer state to FAILED.
-            uploadPartTaskMetadata.state = TransferState.FAILED;
-            dbUtil.updateState(uploadPartRequest.getId(), TransferState.FAILED);
-            LOGGER.error("Encountered error uploading part ", e);
-            throw e;
+                // Check if network is not connected, set the state to WAITING_FOR_NETWORK.
+                try {
+                    if (TransferNetworkLossHandler.getInstance() != null &&
+                            !TransferNetworkLossHandler.getInstance().isNetworkConnected()) {
+                        LOGGER.info("Thread: [" + Thread.currentThread().getId() + "]: Network wasn't available.");
+                        /*
+                         * Network connection is being interrupted. Moving the TransferState
+                         * to WAITING_FOR_NETWORK till the network availability resumes.
+                         */
+                        uploadPartTaskMetadata.state = TransferState.WAITING_FOR_NETWORK;
+                        dbUtil.updateState(uploadPartRequest.getId(), TransferState.WAITING_FOR_NETWORK);
+                        LOGGER.info("Network Connection Interrupted: " +
+                                "Moving the TransferState to WAITING_FOR_NETWORK");
+                        return false;
+                    }
+                } catch (TransferUtilityException transferUtilityException) {
+                    LOGGER.error("TransferUtilityException: [" + transferUtilityException + "]");
+                }
+
+                if (retried >= RETRY_COUNT) {
+                    setTaskState(TransferState.FAILED);
+                    LOGGER.error("Encountered error uploading part ", e);
+                    throw e;
+                }
+                LOGGER.debug("Retry attempt: " + ++retried, e);
+            }
         }
+
+        if (putPartResult == null) {
+            setTaskState(TransferState.FAILED);
+            return false;
+        }
+        setTaskState(TransferState.PART_COMPLETED);
+        dbUtil.updateETag(uploadPartRequest.getId(), putPartResult.getETag());
+        return true;
+    }
+
+    private void setTaskState(TransferState newState) {
+        uploadPartTaskMetadata.state = newState;
+        dbUtil.updateState(uploadPartRequest.getId(),newState);
+    }
+
+    private void resetProgress() {
+        ProgressEvent resetEvent = new ProgressEvent(0);
+        resetEvent.setEventCode(ProgressEvent.RESET_EVENT_CODE);
+        uploadPartTaskProgressListener.progressChanged(resetEvent);
     }
 
     /**
@@ -99,7 +124,7 @@ class UploadPartTask implements Callable<Boolean> {
      */
     private class UploadPartTaskProgressListener implements ProgressListener {
 
-        private UploadTask.UploadTaskProgressListener uploadTaskProgressListener;
+        private final UploadTask.UploadTaskProgressListener uploadTaskProgressListener;
 
         private long bytesTransferredSoFar;
 
@@ -111,15 +136,16 @@ class UploadPartTask implements Callable<Boolean> {
         public void progressChanged(ProgressEvent progressEvent) {
             if (ProgressEvent.RESET_EVENT_CODE == progressEvent.getEventCode()) {
                 // Reset will discard what's been transferred
-                LOGGER.info("Reset Event triggered. Resetting the bytesCurrent to 0.");
+                LOGGER.debug("Reset Event triggered. Resetting the bytesCurrent to 0.");
                 // Reset the local counter to 0.
                 bytesTransferredSoFar = 0;
             } else {
                 bytesTransferredSoFar += progressEvent.getBytesTransferred();          
             }
-            this.uploadTaskProgressListener
-                    .onProgressChanged(UploadPartTask.this.uploadPartRequest.getPartNumber(), 
-                        bytesTransferredSoFar);
+            this.uploadTaskProgressListener.onProgressChanged(
+                    uploadPartRequest.getPartNumber(),
+                    bytesTransferredSoFar
+            );
         }
     }
 }
