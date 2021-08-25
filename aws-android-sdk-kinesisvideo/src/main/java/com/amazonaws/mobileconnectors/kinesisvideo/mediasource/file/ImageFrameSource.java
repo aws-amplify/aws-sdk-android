23737dd6a0707c4e10c6e78c146ea6404c5f06e6
Jeremy Gunawan <jggunawa@amazon.com>
2021-08-25 15:28:17 -0700

    Modify class header comments

--- aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/mobileconnectors/kinesisvideo/mediasource/file/ImageFrameSource.java
+++ aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/mobileconnectors/kinesisvideo/mediasource/file/ImageFrameSource.java
@@ -20,7 +20,8 @@ import static com.amazonaws.kinesisvideo.producer.FrameFlags.FRAME_FLAG_NONE;
 import static com.amazonaws.kinesisvideo.producer.Time.HUNDREDS_OF_NANOS_IN_A_MILLISECOND;
 
 /**
- * Frame source backed by local image files.
+ * Frame source backed by local image files. Once start() is called, this will continuously loop through
+ * all the frames in order.
  */
 public class ImageFrameSource {
     public static final int METADATA_INTERVAL = 8;
