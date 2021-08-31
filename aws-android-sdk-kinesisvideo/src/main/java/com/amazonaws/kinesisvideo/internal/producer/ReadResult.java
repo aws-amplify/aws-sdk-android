05ef12e0651c7307d03ffd5fb0af636a715ea144
Jeremy Gunawan <jggunawa@amazon.com>
2021-08-31 11:50:43 -0700

    Update readresult

--- aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/kinesisvideo/internal/producer/ReadResult.java
+++ aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/kinesisvideo/internal/producer/ReadResult.java
@@ -1,20 +1,3 @@
-/**
- * Copyright 2017-2018 Amazon.com,
- * Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Amazon Software License (the "License").
- * You may not use this file except in compliance with the
- * License. A copy of the License is located at
- *
- *     http://aws.amazon.com/asl/
- *
- * or in the "license" file accompanying this file. This file is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, express or implied. See the License
- * for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.amazonaws.kinesisvideo.internal.producer;
 
 /**
@@ -27,24 +10,17 @@ public class ReadResult {
      */
     public static final long INVALID_UPLOAD_HANDLE_VALUE = -1;
 
-    private long uploadHandle = INVALID_UPLOAD_HANDLE_VALUE;
     private int readBytes = 0;
     private boolean isEndOfStream = false;
 
     /**
      * Setter method which is called from the native codebase.
-     * @param uploadHandle Upload handle
      * @param readBytes Read bytes
      * @param isEndOfStream Whether its the end of stream
      */
-    public void setReadResult(final long uploadHandle, final int readBytes, final boolean isEndOfStream) {
-        this.uploadHandle = uploadHandle;
+    public void setReadResult(final int readBytes, final boolean isEndOfStream) {
         this.readBytes = readBytes;
         this.isEndOfStream = isEndOfStream;
-    }
-
-    public long getUploadHandle() {
-        return uploadHandle;
     }
 
     public int getReadBytes() {
