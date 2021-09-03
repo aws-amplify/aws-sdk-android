c2d3019dbace0ff21152469d15f73ec78a99f16f
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2021-09-03 20:14:29 +0000

    feat(aws-android-sdk-chimesdkidentity): update models to latest

--- aws-android-sdk-chimesdkidentity/src/main/java/com/amazonaws/services/chimesdkidentity/AmazonChimeSDKIdentityClient.java
+++ aws-android-sdk-chimesdkidentity/src/main/java/com/amazonaws/services/chimesdkidentity/AmazonChimeSDKIdentityClient.java
@@ -39,7 +39,9 @@ import com.amazonaws.services.chimesdkidentity.model.transform.*;
  * The Amazon Chime SDK Identity APIs in this section allow software developers
  * to create and manage unique instances of their messaging applications. These
  * APIs provide the overarching framework for creating and sending messages. For
- * more information about the identity APIs, refer to .
+ * more information about the identity APIs, refer to <a href=
+ * "https://docs.aws.amazon.com/chime/latest/APIReference/API_Operations_Amazon_Chime_SDK_Identity.html"
+ * >Amazon Chime SDK identity</a>.
  * </p>
  */
 public class AmazonChimeSDKIdentityClient extends AmazonWebServiceClient implements
