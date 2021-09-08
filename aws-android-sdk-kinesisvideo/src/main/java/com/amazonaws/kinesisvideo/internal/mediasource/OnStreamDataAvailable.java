559cece23412aca2f36ae14a12e7268266efecc8
Jeremy Gunawan <jggunawa@amazon.com>
2021-09-08 11:59:42 -0700

    feat(aws-android-sdk-kinesisvideo): add file media source (#2596)
    * Add file media source
    
    * Add newline to end of files
    
    * Modify class header comments
    
    * Modify class header comments
    
    * Update header comment
    
    * Add more comments

--- aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/kinesisvideo/internal/mediasource/OnStreamDataAvailable.java
+++ aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/kinesisvideo/internal/mediasource/OnStreamDataAvailable.java
@@ -3,9 +3,14 @@ package com.amazonaws.kinesisvideo.internal.mediasource;
 import java.nio.ByteBuffer;
 
 import com.amazonaws.kinesisvideo.common.exception.KinesisVideoException;
+import com.amazonaws.kinesisvideo.producer.KinesisVideoFrame;
 
 public interface OnStreamDataAvailable {
-    void onFrameDataAvailable(final ByteBuffer data) throws KinesisVideoException;
-    void onFragmentMetadataAvailable(final String metadataName, final String metadataValue, final boolean persistent)
-            throws KinesisVideoException;
+    default void onFrameDataAvailable(final ByteBuffer frame) throws KinesisVideoException {
+    }
+    default void onFrameDataAvailable(final KinesisVideoFrame frame) throws KinesisVideoException {
+    }
+    default void onFragmentMetadataAvailable(final String metadataName, final String metadataValue,
+                                             final boolean persistent) throws KinesisVideoException {
+    }
 }
