0ea3b283e36c6014e7fa9aeb69d1961dd33690bd
Rafael Juliano <r_juliano@hotmail.com>
2021-10-21 10:32:48 -0400

    changes for interacting with qos2 server

--- aws-android-sdk-iot/src/main/java/com/amazonaws/mobileconnectors/iot/AWSIotMqttQos.java
+++ aws-android-sdk-iot/src/main/java/com/amazonaws/mobileconnectors/iot/AWSIotMqttQos.java
@@ -1,22 +1,34 @@
 
 package com.amazonaws.mobileconnectors.iot;
 
+import com.amazonaws.services.iotdata.model.InvalidRequestException;
+
 /**
  * Enum to encapsulate the QoS levels allowed by the AWS IoT Service. See:
  * http://docs.aws.amazon.com/iot/latest/developerguide/protocols.html
  */
 public enum AWSIotMqttQos {
-    /** QoS zero. */
     QOS0,
     /** QoS one. */
-    QOS1;
+    QOS1,
+    /** QoS two. */
+    QOS2;
 
     /**
      * Get as an integer to use in MQTT calls requiring an int.
-     * 
+     *
      * @return integer equivalent of the QoS value.
      */
     public int asInt() {
-        return (this == QOS0 ? 0 : 1);
+        switch (this) {
+            case QOS0:
+                return 0;
+            case QOS1:
+                return 1;
+            case QOS2:
+                return 2;
+            default:
+                throw new InvalidRequestException("QoS level not supported.");
+        }
     }
 }
