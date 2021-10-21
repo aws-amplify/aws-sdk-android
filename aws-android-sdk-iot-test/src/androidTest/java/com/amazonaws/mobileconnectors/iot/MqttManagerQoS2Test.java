38579979af85242adfb53bd41f4c3ea11b24cfc7
Rafael Juliano <r_juliano@hotmail.com>
2021-10-21 10:47:20 -0400

    adding tests

---
+++ aws-android-sdk-iot-test/src/androidTest/java/com/amazonaws/mobileconnectors/iot/MqttManagerQoS2Test.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazonaws.mobileconnectors.iot;
+
+import static com.amazonaws.mobileconnectors.iot.MqttManagerTestUtils.GAMMA_ENDPOINT;
+import static com.amazonaws.mobileconnectors.iot.MqttManagerTestUtils.MQTT_CLIENT_ID;
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.amazonaws.services.iot.model.CreateKeysAndCertificateResult;
+
+import org.junit.Test;
+
+import java.security.KeyStore;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+class MqttManagerQoS2Test {
+    private static final String KEYSTORE_NAME = "integration_test.bks";
+    private static final String KEYSTORE_PASSWORD = "test password";
+
+    @Test
+    public void mqttConnect_test_qos2_GammaEndpoint() throws Exception {
+        Context appContext = ApplicationProvider.getApplicationContext();
+        String keyStorePath = appContext.getFilesDir().toString() + "/";
+        System.out.println(keyStorePath);
+        final CountDownLatch connectionLatch = new CountDownLatch(1);
+        final CountDownLatch publishMessageLatch = new CountDownLatch(1);
+        final CountDownLatch receiveMessageLatch = new CountDownLatch(1);
+        final AWSIotMqttManager mqttManager = new AWSIotMqttManager(MQTT_CLIENT_ID + "-QOS2",
+                                                                    GAMMA_ENDPOINT);
+        // Initialize this with the value returned by a call to CreateKeyAndCertificate API or CLI call
+        // Once it's live in prod, we can remove this since the test in MqttManagerIntegrationTest will pass.
+        CreateKeysAndCertificateResult createKeysAndCertificateResult = null;
+
+        if (!AWSIotKeystoreHelper.isKeystorePresent(keyStorePath, KEYSTORE_NAME)) {
+            IotClient.KeysAndCertificateInfo keysAndCertificateInfo = new IotClient.KeysAndCertificateInfo(createKeysAndCertificateResult);
+
+            AWSIotKeystoreHelper.saveCertificateAndPrivateKey(
+                keysAndCertificateInfo.getCertificateId(),
+                keysAndCertificateInfo.getCertificatePem(),
+                keysAndCertificateInfo.getPrivateKey(),
+                keyStorePath,
+                KEYSTORE_NAME,
+                KEYSTORE_PASSWORD
+            );
+
+        }
+        final KeyStore keyStore = AWSIotKeystoreHelper.getIotKeystore(
+            createKeysAndCertificateResult.getCertificateId(),
+            keyStorePath,
+            KEYSTORE_NAME,
+            KEYSTORE_PASSWORD
+        );
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mqttManager.connect(keyStore, new MqttManagerTestUtils.LatchedAWSIotMqttClientStatusCallback(connectionLatch));
+            }
+        }).start();
+
+        connectionLatch.await(20, TimeUnit.SECONDS);
+        assertEquals(0, connectionLatch.getCount());
+
+        String messageBody = "Testing QOS2";
+        mqttManager.publishString(messageBody, "/mytopic", AWSIotMqttQos.QOS2, new AWSIotMqttMessageDeliveryCallback() {
+            @Override
+            public void statusChanged(MessageDeliveryStatus status, Object userData) {
+                publishMessageLatch.countDown();
+            }
+        }, null, true);
+
+        publishMessageLatch.await(10, TimeUnit.SECONDS);
+        assertEquals(0, publishMessageLatch.getCount());
+
+        final AtomicReference<byte[]> result = new AtomicReference<>();
+        mqttManager.subscribeToTopic("/mytopic", AWSIotMqttQos.QOS2, new AWSIotMqttNewMessageCallback() {
+            @Override
+            public void onMessageArrived(String topic, byte[] data) {
+                result.set(data);
+                receiveMessageLatch.countDown();
+            }
+        });
+
+        receiveMessageLatch.await(10, TimeUnit.SECONDS);
+
+        assertEquals(messageBody, new String(result.get()));
+
+        mqttManager.disconnect();
+    }
+}
---
+++ aws-android-sdk-iot-test/src/androidTest/java/com/amazonaws/mobileconnectors/iot/MqttManagerTestUtils.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.amazonaws.mobileconnectors.iot;
+
+import android.os.Build;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.concurrent.CountDownLatch;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+
+class MqttManagerTestUtils {
+    public static final String GAMMA_ENDPOINT = "<some test endpoint";
+    public static final String MQTT_CLIENT_ID = "TEST-MQTT-CLIENT";
+
+    /**
+     *
+     * @param selfSignedRootCertPfxPath
+     * @param password
+     * @return
+     * @throws KeyStoreException
+     * @throws CertificateException
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     * @throws KeyManagementException
+     */
+    public static SSLSocketFactory getSSLSocketFactory(String selfSignedRootCertPfxPath, String password)
+        throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, KeyManagementException {
+
+        FileInputStream fip = new FileInputStream(selfSignedRootCertPfxPath);
+        SSLContext ctx = null;
+        SSLSocketFactory sslSockFactory=null;
+        KeyStore ks;
+        ks = KeyStore.getInstance("PKCS12");
+        ks.load(fip, password.toCharArray());
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+        tmf.init(ks);
+        TrustManager[] tm = tmf.getTrustManagers();
+        ctx = SSLContext.getInstance("TLS");
+        ctx.init(null, tm, null);
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            sslSockFactory = new TLSSocketFactory(tm);
+        } else {
+            sslSockFactory = ctx.getSocketFactory();
+        }
+        return sslSockFactory;
+
+
+    }
+
+    static final class LatchedAWSIotMqttClientStatusCallback implements AWSIotMqttClientStatusCallback {
+        private final CountDownLatch latch;
+        private static final String TAG = LatchedAWSIotMqttClientStatusCallback.class.getSimpleName();
+
+        public LatchedAWSIotMqttClientStatusCallback(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void onStatusChanged(AWSIotMqttClientStatus status, Throwable throwable) {
+            Log.d(TAG, "Connection status received: " + status.name());
+            if (throwable != null) {
+                Log.e(TAG, "EXCEPTION: " + throwable.getMessage(), throwable );
+            }
+            if (AWSIotMqttClientStatus.Connected.equals(status)) {
+                latch.countDown();
+            }
+        }
+    }
+
+
+    static final class TLSSocketFactory extends SSLSocketFactory {
+
+        private final SSLSocketFactory internalSSLSocketFactory;
+
+        public TLSSocketFactory(TrustManager[] trustManagers) throws KeyManagementException, NoSuchAlgorithmException {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, trustManagers, null);
+            internalSSLSocketFactory = context.getSocketFactory();
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return internalSSLSocketFactory.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return internalSSLSocketFactory.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket() throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket());
+        }
+
+        @Override
+        public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(s, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress host, int port) throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+            return enableTLSOnSocket(internalSSLSocketFactory.createSocket(address, port, localAddress, localPort));
+        }
+
+        private Socket enableTLSOnSocket(Socket socket) {
+            if(socket != null && (socket instanceof SSLSocket)) {
+                ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1.2", "TLSv1.1"});
+            }
+            return socket;
+        }
+    }
+}
