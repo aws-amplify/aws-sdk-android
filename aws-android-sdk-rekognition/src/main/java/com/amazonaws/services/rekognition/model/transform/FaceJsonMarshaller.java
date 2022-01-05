cd85664b09c69ce404c2f498a1cdb14b49357cac
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2022-01-05 11:30:24 -0800

    feat(aws-android-sdk-rekognition): update models to latest (#2759)

--- aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/model/transform/FaceJsonMarshaller.java
+++ aws-android-sdk-rekognition/src/main/java/com/amazonaws/services/rekognition/model/transform/FaceJsonMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -50,6 +50,11 @@ class FaceJsonMarshaller {
             Float confidence = face.getConfidence();
             jsonWriter.name("Confidence");
             jsonWriter.value(confidence);
+        }
+        if (face.getIndexFacesModelVersion() != null) {
+            String indexFacesModelVersion = face.getIndexFacesModelVersion();
+            jsonWriter.name("IndexFacesModelVersion");
+            jsonWriter.value(indexFacesModelVersion);
         }
         jsonWriter.endObject();
     }
