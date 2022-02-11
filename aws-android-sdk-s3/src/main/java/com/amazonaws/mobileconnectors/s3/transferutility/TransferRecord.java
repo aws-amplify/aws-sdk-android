e3e940b4a97dfe09affa370c9a0a49606eac7e6e
Anshul <gpanshu@amazon.com>
2022-02-11 11:38:19 -0500

    Adding the null guard when getTransferNetworkConnectionType is null

--- aws-android-sdk-s3/src/main/java/com/amazonaws/mobileconnectors/s3/transferutility/TransferRecord.java
+++ aws-android-sdk-s3/src/main/java/com/amazonaws/mobileconnectors/s3/transferutility/TransferRecord.java
@@ -364,9 +364,8 @@ class TransferRecord {
             // Unable to get the details of the network, we will start the transfer.
             return true;
         }
-            
-        if (transferUtilityOptions != null &&
-            !transferUtilityOptions.getTransferNetworkConnectionType().isConnected(connManager)) {
+
+        if (transferUtilityOptions != null && transferUtilityOptions.getTransferNetworkConnectionType() != null && !transferUtilityOptions.getTransferNetworkConnectionType().isConnected(connManager)) {
             // the network that is configured in the TransferUtilityOptions is not available.
             // we will set the state to WAITING_FOR_NETWORK. The transfer will be started
             // when a future notification is received for the desired network.
