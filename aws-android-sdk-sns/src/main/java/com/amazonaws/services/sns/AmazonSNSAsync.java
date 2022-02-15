1d3f53d65cf9c22307bb8d0fb23a6f4e1eb82dd3
AWS Mobile SDK Team <46607340+awsmobilesdk@users.noreply.github.com>
2022-02-15 10:53:55 -0500

    feat(aws-android-sdk-sns): update models to latest (#2795)

--- aws-android-sdk-sns/src/main/java/com/amazonaws/services/sns/AmazonSNSAsync.java
+++ aws-android-sdk-sns/src/main/java/com/amazonaws/services/sns/AmazonSNSAsync.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -2136,7 +2136,7 @@ public interface AmazonSNSAsync extends AmazonSNS {
      * </p>
      * <p>
      * When a <code>messageId</code> is returned, the message is saved and
-     * Amazon SNS immediately deliverers it to subscribers.
+     * Amazon SNS immediately delivers it to subscribers.
      * </p>
      * <p>
      * To use the <code>Publish</code> action for publishing a message to a
@@ -2202,7 +2202,7 @@ public interface AmazonSNSAsync extends AmazonSNS {
      * </p>
      * <p>
      * When a <code>messageId</code> is returned, the message is saved and
-     * Amazon SNS immediately deliverers it to subscribers.
+     * Amazon SNS immediately delivers it to subscribers.
      * </p>
      * <p>
      * To use the <code>Publish</code> action for publishing a message to a
