d9646be302eaae7a17f05ebb8a825edac702ba27
Tyler Roach <tjroach@amazon.com>
2023-04-07 11:56:29 -0400

    Include SDK 21 in fix

--- aws-android-sdk-core/src/main/java/com/amazonaws/http/UrlHttpClient.java
+++ aws-android-sdk-core/src/main/java/com/amazonaws/http/UrlHttpClient.java
@@ -55,13 +55,13 @@ public class UrlHttpClient implements HttpClient {
     private static final int BUFFER_SIZE_MULTIPLIER = 8;
     private final ClientConfiguration config;
 
-    // SocketFactory for Pre SDK 21 devices to enforce TLS 1.2
+    // SocketFactory for Pre SDK 22 devices to enforce TLS 1.2
     private final TLS12SocketFactory tls12SocketFactory;
 
     // Cached SSLContext for connections using custom TrustManagers.
     private SSLContext customTrustSSLContext = null;
 
-    // SocketFactory for Pre SDK 21 devices to enforce TLS 1.2 that also holds custom TrustManagers.
+    // SocketFactory for Pre SDK 22 devices to enforce TLS 1.2 that also holds custom TrustManagers.
     private TLS12SocketFactory customTrustTls12SocketFactory;
 
     /**
@@ -71,7 +71,7 @@ public class UrlHttpClient implements HttpClient {
     public UrlHttpClient(ClientConfiguration config) {
         this.config = config;
 
-        // will return null if SDK > 21
+        // will return null if SDK > 22
         tls12SocketFactory = TLS12SocketFactory.createTLS12SocketFactory();
     }
 
