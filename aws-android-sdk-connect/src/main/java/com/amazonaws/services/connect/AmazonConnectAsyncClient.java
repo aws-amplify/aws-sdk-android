7fa106254c3a62336819abbe1ece105a2753fd2b
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2023-05-11 18:05:43 +0000

    feat(aws-android-sdk-connect): update models to latest

--- aws-android-sdk-connect/src/main/java/com/amazonaws/services/connect/AmazonConnectAsyncClient.java
+++ aws-android-sdk-connect/src/main/java/com/amazonaws/services/connect/AmazonConnectAsyncClient.java
@@ -7183,7 +7183,7 @@ public class AmazonConnectAsyncClient extends AmazonConnectClient implements Ama
      * >GetMetricData</a>, the previous version of this API. It has new metrics,
      * offers filtering at a metric level, and offers the ability to filter and
      * group data by channels, queues, routing profiles, agents, and agent
-     * hierarchy levels. It can retrieve historical data for the last 14 days,
+     * hierarchy levels. It can retrieve historical data for the last 35 days,
      * in 24-hour intervals.
      * </p>
      * <p>
@@ -7234,7 +7234,7 @@ public class AmazonConnectAsyncClient extends AmazonConnectClient implements Ama
      * >GetMetricData</a>, the previous version of this API. It has new metrics,
      * offers filtering at a metric level, and offers the ability to filter and
      * group data by channels, queues, routing profiles, agents, and agent
-     * hierarchy levels. It can retrieve historical data for the last 14 days,
+     * hierarchy levels. It can retrieve historical data for the last 35 days,
      * in 24-hour intervals.
      * </p>
      * <p>
@@ -13928,6 +13928,13 @@ public class AmazonConnectAsyncClient extends AmazonConnectClient implements Ama
      * </p>
      * <important>
      * <p>
+     * After using this API, you must verify that the phone number is attached
+     * to the correct flow in the target instance or traffic distribution group.
+     * You need to do this because the API switches only the phone number to a
+     * new instance or traffic distribution group. It doesn't migrate the flow
+     * configuration of the phone number, too.
+     * </p>
+     * <p>
      * You can call <a href=
      * "https://docs.aws.amazon.com/connect/latest/APIReference/API_DescribePhoneNumber.html"
      * >DescribePhoneNumber</a> API to verify the status of a previous <a href=
@@ -13971,6 +13978,13 @@ public class AmazonConnectAsyncClient extends AmazonConnectClient implements Ama
      * or traffic distribution group in the same Amazon Web Services Region.
      * </p>
      * <important>
+     * <p>
+     * After using this API, you must verify that the phone number is attached
+     * to the correct flow in the target instance or traffic distribution group.
+     * You need to do this because the API switches only the phone number to a
+     * new instance or traffic distribution group. It doesn't migrate the flow
+     * configuration of the phone number, too.
+     * </p>
      * <p>
      * You can call <a href=
      * "https://docs.aws.amazon.com/connect/latest/APIReference/API_DescribePhoneNumber.html"
