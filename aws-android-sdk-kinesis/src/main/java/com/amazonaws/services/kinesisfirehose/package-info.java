c01c1bf1ff9dba32f7f12271fcbc5728a1520136
awsmobilesdk <aws-mobile-sdk-dev@amazon.com>
2024-02-20 19:05:06 +0000

    feat(aws-android-sdk-kinesis): update models to latest

--- aws-android-sdk-kinesis/src/main/java/com/amazonaws/services/kinesisfirehose/package-info.java
+++ aws-android-sdk-kinesis/src/main/java/com/amazonaws/services/kinesisfirehose/package-info.java
@@ -1,5 +1,5 @@
 /**
- * <fullname>Amazon Kinesis Data Firehose API Reference</fullname> <p>Amazon Kinesis Data Firehose is a fully managed service that delivers real-time streaming data to destinations such as Amazon Simple Storage Service (Amazon S3), Amazon OpenSearch Service, Amazon Redshift, Splunk, and various other supportd destinations.</p>
+ * <fullname>Amazon Data Firehose</fullname> <p>Amazon Data Firehose is a fully managed service that delivers real-time streaming data to destinations such as Amazon Simple Storage Service (Amazon S3), Amazon OpenSearch Service, Amazon Redshift, Splunk, and various other supportd destinations.</p>
  */
 
 package com.amazonaws.services.kinesisfirehose;
