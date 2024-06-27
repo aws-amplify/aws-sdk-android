212f5552c36889e10065a3eff7cf888c863c669c
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-06-27 14:32:29 -0700

    Revert "Revert EncoderFactory color format changes"
    This reverts commit bfd9940e216731e55bb1cd112928b72a4a845e9a.

--- aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/mobileconnectors/kinesisvideo/encoding/EncoderFactory.java
+++ aws-android-sdk-kinesisvideo/src/main/java/com/amazonaws/mobileconnectors/kinesisvideo/encoding/EncoderFactory.java
@@ -45,26 +45,25 @@ public class EncoderFactory {
     private static MediaCodec createMediaCodec(final CameraMediaSourceConfiguration mediaSourceConfiguration) {
         try {
             final MediaCodec encoder = MediaCodec.createEncoderByType(mediaSourceConfiguration.getEncoderMimeType());
-            try {
-                encoder.configure(
-                        configureMediaFormat(mediaSourceConfiguration,
-                                MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar),
-                        NULL_SURFACE,
-                        NULL_CRYPTO,
-                        MediaCodec.CONFIGURE_FLAG_ENCODE);
-                logSupportedColorFormats(encoder, mediaSourceConfiguration);
-            } catch (MediaCodec.CodecException e) {
-                Log.d(TAG, "Failed configuring MediaCodec with Semi-planar pixel format, falling back to planar");
+            // try {
+                // encoder.configure(
+                //         configureMediaFormat(mediaSourceConfiguration,
+                //                 MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar),
+                //         NULL_SURFACE,
+                //         NULL_CRYPTO,
+                //         MediaCodec.CONFIGURE_FLAG_ENCODE);
+                // logSupportedColorFormats(encoder, mediaSourceConfiguration);
+            // } catch (MediaCodec.CodecException e) {
+                // Log.d(TAG, "Failed configuring MediaCodec with Semi-planar pixel format, falling back to planar");
 
                 encoder.configure(
                         configureMediaFormat(mediaSourceConfiguration,
-                                // MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible), // NOTE: COLOR_FormatYUV420SemiPlanar and COLOR_FormatYUV420Planar are deprecated
-                                MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar),
+                                MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible),
                         NULL_SURFACE,
                         NULL_CRYPTO,
                         MediaCodec.CONFIGURE_FLAG_ENCODE);
                 logSupportedColorFormats(encoder, mediaSourceConfiguration);
-            }
+            // }
 
             return encoder;
         } catch (final IOException e) {
